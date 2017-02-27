c8e63846b1426a2892a0358f1201ab3e2ec11e08
Xmetalfanx <Xmetalfanx@yahoo.com>
2017-02-27 05:12:07 -0500

    work on converting the browser section
    converting to jade and bootstrap 4

---
+++ techHQ/browsers/adblocking/variables.php
@@ -0,0 +1,27 @@
+<?php
+
+      // Icons
+      $ublockIcon = 'img(src='/techHQ/browsers/adblocking/icons/ublockO.png' alt='Ublock Origin Icon')';
+      $adguardIcon = 'img(src='/techHQ/browsers/adblocking/icons/adguard.png' alt='Adguard Icon')';
+      $adblockEdgeIcon = 'img(src='/techHQ/browsers/adblocking/icons/abe.png' alt='Adblock Edge Icon')';
+      $firefoxIcon = 'img(src='/graphics/progIcons/browsers/firefox_1.png' alt='Firefox Icon') Firefox';
+
+
+      // Note the issues with all the quotes inside quotes below
+      // Links
+
+            // Firefox
+            $uBlockOFirefox = 'a(href='https://addons.mozilla.org/en-us/firefox/addon/ublock-origin/') Ublock Origin';
+            $adGuardFirefox = 'a(href='https://addons.mozilla.org/en-us/firefox/addon/adguard-adblocker/') Ad-guard';
+            $ABEFirefox = 'a(href='https://addons.mozilla.org/en-us/firefox/addon/adblock-edge/') AdBlock Edge';
+            $EHFirefox = 'a(href='https://addons.mozilla.org/en-us/firefox/addon/elemhidehelper/') Element Hider Add-on for Adblock Edge';
+
+            // Chrome
+           $UBlockOriginChrome = 'a(href='https://chrome.google.com/webstore/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm?hl=en') Ublock Origin';
+
+           $AdguardChrome = 'a(href='https://chrome.google.com/webstore/detail/adguard-adblocker/bgnkhhnnamicmpeenaelnjfhikgbkllg?hl=pl') Adguard';
+
+           // Opera
+           $uBlockOriginOpera = 'a(href='https://addons.opera.com/en/extensions/details/ublock/?display=en') Ublock Origin';
+           $adGuardOpera = 'a(href='https://addons.opera.com/en/extensions/details/adguard/?display=en') Adguard';
+ ?>
