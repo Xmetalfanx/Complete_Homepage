ab3e7734157374e4c3afcf766066b5bd4cedafe5
Xmetalfanx <Xmetalfanx@yahoo.com>
2017-03-09 16:35:27 -0500

    changed some more of the Firefox Addon Section to Jade
    TODO: Finish the conversion and moving the jade to html into the main pages
    
    TODO: Added/Figure out how to add Chrome Addons

--- techHQ/browsers/DONE4NOW/markdown/browserIndex.md
+++
@@ -1,6 +0,0 @@
-##Browser Section Intro
-This is the New Browers Section.
-
-This section will contain infomation on browser addons and what ones I find most useful to improve your webbrowsing experience.  
-
-This will not just Firefox as it use to be on my site, but will also contain Opera, Chrom* (Chromium and Google Chrome) browser info as well as new browsers such as Vivadi 
--- techHQ/software/websiteProgs.php
+++ techHQ/software/websiteProgs.php
@@ -5,29 +5,33 @@
 
 ?>
 
+
     <p class="header">WebSite Tools</p>
-    <p>These  three for the record are three of the programs I use most, while working on my site.    </p>
+    
+    <p>These three for the record are three of the programs I use most, while working on my site.    </p>
+   
     <ul>
-      <li><a href="http://www.macromedia.com" target="_blank">
-      <img src="../progIcons/product/Dreamweaver_433.png" alt="Dreamweaver" width="48" height="48" class="iconsLeft" /></a>    </li>
+      <li>
+      <a href="http://www.macromedia.com" target="_blank">
+      <img src="../progIcons/product/Dreamweaver_433.png" alt="Dreamweaver"/></a>    
+      </li>
 
       <li><a href="http://www.macromedia.com" target="_blank">
       <span class="progName">Dreamweaver</span></a> - <span class="justify">The Best Web Editor/Designer/Coder Program on the web</span></li>
     </ul>
     <ul>
       <li><a href="http://www.htmlvalidator.com/" target="_blank">
-      <img src="../progIcons/product/cse90_MAINICON.png" alt="CSE Validator Icon" width="36" height="36" class="iconsLeft" />
+      <img src="../progIcons/product/cse90_MAINICON.png" alt="CSE Validator Icon"/>
       <span class="progName">CSE HTML Validator</span></a> - Great Website Syntax Checker that catches many many mistakes in coding that other syntax checkers misses    </li>
     </ul>
     <ul>
-      <li><a href="http://www.bradsoft.com/" target="_blank">
-      <img src="../progIcons/product/TopStyle4_AAA_APP_ICON.png" alt="Top Styles Icon" width="27" height="27" class="iconsLeft" /></a></li>
+      <li>
+      <a href="http://www.bradsoft.com/" target="_blank">
+      <img src="../progIcons/product/TopStyle4_AAA_APP_ICON.png" alt="Top Styles Icon"  /></a>
+      </li>
 
       <li><a href="http://www.bradsoft.com/" target="_blank" class="progName"> Top Styles  </a> -
        Great program for site coding/checking/and optimizations ... A must have for anyone doing CSS, ... like me
        <img src="../smiles/meeting.gif" alt="Ta-Da Smile" width="53" height="34" class="center" /></li>
+    
     </ul>
-
-EOD;
-?>
-<?php include $_SERVER['DOCUMENT_ROOT'].("/templates/tech/baseTechSoftwareTemplate.php"); ?>
