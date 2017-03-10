3b65bfad8f51f78eced67081aaea8da60f98fb00
Xmetalfanx <Xmetalfanx@yahoo.com>
2017-03-09 19:27:22 -0500

    Pruning and Jade conversions in the TechHQ Security Section

--- techHQ/modules/variables/programs.php
+++ techHQ/modules/variables/programs.php
@@ -1,38 +1,37 @@
 <?php
 
-      $kritaIcon = '<img src="/techHQ/progIcons/graphics/krita.png" alt="Krita Icon">';
-      $kritaHome = '<a href="https://krita.org/" target="_blank">Krita</a>';
+  $kritaIcon = '<img src="/techHQ/progIcons/graphics/krita.png" alt="Krita Icon">';
+  $kritaHome = '<a href="https://krita.org/" target="_blank">Krita</a>';
 
-      $gimpIcon = '<img src="/techHQ/progIcons/graphics/gimp.png" alt="GIMP Icon">';
-      $gimeHome =   '<a href="https://www.gimp.org/" target="_blank">GIMP</a>';
+  $gimpIcon = '<img src="/techHQ/progIcons/graphics/gimp.png" alt="GIMP Icon">';
+  $gimeHome =   '<a href="https://www.gimp.org/" target="_blank">GIMP</a>';
 
-      $xnviewIcon = '<img src="/techHQ/progIcons/graphics/XnView.png" alt="Xnview Icon">';
-      $xnviewHome = '<a href="http://www.xnview.com/" target="_blank">XnView</a>';
+  $xnviewIcon = '<img src="/techHQ/progIcons/graphics/XnView.png" alt="Xnview Icon">';
+  $xnviewHome = '<a href="http://www.xnview.com/" target="_blank">XnView</a>';
 
-      $irfanViewIcon = '<img src="/techHQ/progIcons/graphics/17__irfanview.png" alt="Irfan View Icon">';
-      $irfanViewHome = '<a href="http://www.irfanview.com/" target="_blank">IrfanView</a>';
+  $irfanViewIcon = '<img src="/techHQ/progIcons/graphics/17__irfanview.png" alt="Irfan View Icon">';
+  $irfanViewHome = '<a href="http://www.irfanview.com/" target="_blank">IrfanView</a>';
 
-      $libreOIcon = '<img src="/techHQ/progIcons/product/libreIcons/lofficeMain.png" alt="Libre Office Icon">';
-      $libreOHome = '<a href="https://www.libreoffice.org/">Libre Office</a> - Microsoft Office Alternative';
+  $libreOIcon = '<img src="/techHQ/progIcons/product/libreIcons/lofficeMain.png" alt="Libre Office Icon">';
+  $libreOHome = '<a href="https://www.libreoffice.org/">Libre Office</a> - Microsoft Office Alternative';
 
-	  $foxITIcon = '<img src="/techHQ//progIcons/product/pdf/foxit.png" alt="Foxit Reader Icon">';
-      $foxITHome = '<a href="http://www.foxitsoftware.com/">Foxit Reader</a>';
+  $foxITIcon = '<img src="/techHQ//progIcons/product/pdf/foxit.png" alt="Foxit Reader Icon">';
+  $foxITHome = '<a href="http://www.foxitsoftware.com/">Foxit Reader</a>';
 
-      $sumPDFIcon =  '<img src="/techHQ/progIcons/product/pdf/SumatraPDF_1.png" alt="SumatraPDF Icon">';
-      $sumPDFHome =  '  <a href="http://blog.kowalczyk.info/software/sumatrapdf">SumatraPDF</a>';
+  $sumPDFIcon =  '<img src="/techHQ/progIcons/product/pdf/SumatraPDF_1.png" alt="SumatraPDF Icon">';
+  $sumPDFHome =  '  <a href="http://blog.kowalczyk.info/software/sumatrapdf">SumatraPDF</a>';
 
-      $pdfXIcon = '<img src="/techHQ/progIcons/product/pdf/PDFXCview_128.png" alt="PDF XViewer">';
-      $pdfXHome = '<a href="http://www.tracker-software.com/product/pdf-xchange-viewer">PDF XViewer</a>';
+  $pdfXIcon = '<img src="/techHQ/progIcons/product/pdf/PDFXCview_128.png" alt="PDF XViewer">';
+  $pdfXHome = '<a href="http://www.tracker-software.com/product/pdf-xchange-viewer">PDF XViewer</a>';
 
+  $ccleanerIcon = '<img src="/techHQ/progIcons/security/cleaners/959__CCv2.gif" alt="CCleaner Icon">';
+  $ccleanerHome = ' <a href="http://www.ccleaner.com/">CCleaner</a>';
 
-	  $ccleanerIcon = '<img src="/techHQ/progIcons/security/cleaners/959__CCv2.gif" alt="CCleaner Icon">';
-      $ccleanerHome = ' <a href="http://www.ccleaner.com/">CCleaner</a>';
+  $CCEnhIcon = '<img src="/techHQ/progIcons/security/cleaners/CCEnhancer_32512.png" alt="CCEnhancer Icon">';
+  $CCEnhHome = '<a href="http://majorgeeks.com/CCEnhancer_d6547.html">CCEnhancer</a>';
 
-      $CCEnhIcon = '<img src="/techHQ/progIcons/security/cleaners/CCEnhancer_32512.png" alt="CCEnhancer Icon">';
-      $CCEnhHome = '<a href="http://majorgeeks.com/CCEnhancer_d6547.html">CCEnhancer</a>';
-
-  	 $glaryIcon = '<img src="/techHQ/progIcons/system_tools/glary.png" alt="Glary Utilities Icon">';
-     $glaryHome = '<a href="http://www.glarysoft.com/">Glary Utilties</a>';
+	 $glaryIcon = '<img src="/techHQ/progIcons/system_tools/glary.png" alt="Glary Utilities Icon">';
+   $glaryHome = '<a href="http://www.glarysoft.com/">Glary Utilties</a>';
 
     $wiseCareIcon = '<img src="/techHQ/progIcons/system_tools/WiseCare365_MAINICON.png" alt="Wise Care 360 Icon">';
     $wiseCareHome = '<a href="http://www.wisecleaner.com/wise-care-365.html" target="_blank">Wise Care 360</a>';
@@ -42,7 +41,6 @@
 
     $smPlayerIcon = '<img src="/techHQ/progIcons/media_players/smplayer_logo.png" alt="SMPlayer Icon">';
     $smPlayerHome = '<a href="http://smplayer.sourceforge.net/">SMPlayer</a>';
-
 
     $vlcIcon = '<img src="/techHQ/progIcons/media_players/vlc.png" alt="VLC icon" />';
     $vlcHome = '<a href="http://www.videolan.org/vlc/index.html">VLC</a>';
@@ -65,13 +63,9 @@
     $inkscapeIcon = '<img src="/techHQ/progIcons/graphics/inkscape_APPLICATION_ICON.png" alt="InkScape Icon" />';
     $inkscapeHome = '<a href="http://www.inkscape.org/ target="_blank"> Mozillla Thunderbird </a>';
 
-
-
     $majorGeeksIcon = '<a href="http://www.majorgeeks.com" target="_blank">
                         <img src="/techHQ/graphics/portals/mg_logo_trns.gif" alt="Major Geeks Icon" /></a>';
 
-
     $softpediaIcon = ' <a href="http://www.softpedia.com" target="_blank">
                       <img src="/techHQ/graphics/portals/softpedia_logo.gif" alt="Softpedia Graphics" /> </a>';
-
  ?>
--- techHQ/security/ads/popups.php
+++ techHQ/security/ads/popups.php
@@ -1,96 +1,69 @@
-<?php 
-      $the_title = 'Xmetalfanx Security Section - Popup Blockers';
-		$the_content = <<<EOD
-    
-        <p>
-          Ok.. here is a page I have let get away from me ... oh well ... lets
-          start over anyway        </p>
-        <p class="boldTitles">
-          Popup Blockers        </p>
-        <p>
-          I will not waste anyone&#39;s time with &quot;what is a popup&quot;
-          ... everyone on the net has seen them, and to be honest, NOT ALL
-          POPUP BLOCKERS are alike... alot claim to block stuff, but need a TON
-          of customizing and they ... STILL DONT WORK ALL THE TIME ... well
-          don&#39;t get ... </p>
-        <table width="68%" border="0" align="center" cellpadding="0" cellspacing="0">
-          <tr>
-            <td width="25%">
-				<img src="../../../smiles/funny/lamer2.gif" alt="pc hits back smile" width="60" height="23" />
-			</td>
-            <td width="25%" class="center"><img src="../../../smiles/emoticons/emot151.gif" alt="Mad Driver" width="60" height="60" /></td>
-            <td width="25%" class="center">
-			<img src="../../../smiles/various/throw.gif" alt="throw your pc against the wall" width="72" height="43" hspace="4" /></td>
-            <td width="25%" class="center">
-			<img src="../../../smiles/mad/ranting.gif" alt="Ranting" width="28" height="24" />
-			</td>
-          </tr>
-      </table>
-<p>... get informed </p>
-        <p class="header">
-          There have been three popup blockers I have used/use that I give a
-          thumbs up        </p>
-        <ul>
-          <li class="justify">
-            <strong>Popup Killer (FW/Open Source)</strong> - In the days of
-            windows 9x, Loved this program... its good, configurrable. you can
-            save blacklists and whitelists... but to be honest, they stopped
-            development and the open Source version i tried, .... kind of fell
-            flat.... no so much &quot;cause this program sucks&quot;, but
-            because popups have gotten more advanced to get around those old
-            blockers.<br />
-          </li>
-          <li>
-            <strong><a href="http://www.popupcop.com" target="_blank">Popup Cop (SW)</a></strong> - here is one if you use IE (not sure about IE
-            clone) as your default browser, but it will not work in any other
-            browser . .. it has a
-            TOOOOOOOOOOOOOOOOON of options, and features,... i could probably
-            make a whole site (not page) just to describe it ... alot of the
-            previously mentioned &quot;smart popups&quot; can be detected and
-            removed without any user interaction... if you want a popup (say a
-            prompt on a site is being blocked... one you know you need to see),
-            just click enable popups on this site          </li>
-        </ul>
-        <p><a href="http://www.admuncher.com" target="_blank"><img src="../../../progIcons/security/ad_muncher.png" alt="Ad Muncher Icon" width="31" height="31" class="iconsLeft" /></a></p>
-        <ul>
-          <li><strong><a href="http://www.admuncher.com" target="_blank">Ad Muncher</a></strong>-
-            This works in all programs... from web browsers IE, Opera, Mozilla,
-            Firefox, (any IE clone), Paltalk... and although i have not tried
-            it this way, Yahoo messanger and adware programs.... This is a
-            GREAT TOOL, that although I love Popup Cop... I really do... this
-            is just a notch or two above ... This blocks alot like Popup Cop...
-            it doesn&#39;t need alot of user interaction, but if you need to
-            set it up so it doesn&#39;t block on a certain site, its a piece of
-            cake to do...          </li>
-        </ul>
-<p>Many non-IE (thats counting IE clones) have built in popup blockers that do very well .. I do not have the URL at the moment, but several sites put 3<sup>rd</sup> party popup blockers up
- against some of the built in blockers and the built in blockers rated much higher. All of these (if you have one that doesn't ... you do not have a good one) have exception lists, so if 
- you find its blocking too much... just tell it not to block a particular item ... OR on a particular page </p>
-        <p class="header">&quot;Non Internet Explorer Browsers &quot; that have great popup blockers </p>
-        <ul>
-          <li><span class="boldUnderline">Internet Explorer Clones</span>
-            <ul>
-              <li><span class="boldUnderline"><a href="http://www.maxthon.com" target="_blank">Maxthon</a></span>
-                - Free IE Clone with a TON more options (BTW ... i would download and use maxthon before downloading IE7 ... 
-                IE7 doesn't even COMPARE to the features of Maxthon
-              </li>
-            </ul>
-          </li>
-          <ul>
-            <li><span class="boldUnderline"><a href="http://www.avant.com">Avant</a> </span>
-              - Not to be outdone ... this is VERY simple to Maxthon ... but yet different in some ways ... 
-              try both and see for yourself</li>
-          </ul>
-          <li><span class="boldUnderline"><a href="http://www.firefox.com" target="_blank"><img src="../../progIcons/browsers/firefox_1.png" alt="Firefox Icon" width="36" height="36" class="iconsLeft" /> Firefox</a></span>
-            - This is the biggest (IMHO) alternative to IE right now ... not counting when I am testing my site in
-          other browsers, this is the one I use all the time for regular use. </li>
-          
+<?php  $the_title = 'Xmetalfanx Security Section - Popup Blockers'; ?>
+<p>
+  Ok.. here is a page I have let get away from me ... oh well ... lets
+  start over anyway
+</p>
+<p class="boldTitles">Popup Blockers</p>
+<p>
+  I will not waste anyone&apos;s time with &quot;what is a popup&quot;
+  ... everyone on the net has seen them, and to be honest, NOT ALL
+  POPUP BLOCKERS are alike... alot claim to block stuff, but need a TON
+  of customizing and they ... STILL DONT WORK ALL THE TIME ... well
+  don&apos;t get ...
+</p>
+<div class="container">
+  <div class="col-lg-3"></div><img src="../../../smiles/funny/lamer2.gif" alt="pc hits back smile" width="60" height="23"/>
+  <div class="col-lg-3"><img src="../../../smiles/emoticons/emot151.gif" alt="Mad Driver" width="60" height="60"/></div>
+  <div class="col-lg-3"><img src="../../../smiles/various/throw.gif" alt="throw your pc against the wall" width="72" height="43"/></div>
+  <div class="col-lg-3"><img src="../../../smiles/mad/ranting.gif" alt="Ranting" width="28" height="24"/></div>
+</div>
+<p>... get informed</p>
+<p class="header">There have been three popup blockers I have used/use that I give a thumbs up</p>
+<ul>
+  <li class="justify"><strong>Popup Killer (FW/Open Source)</strong> - In the days of
+        windows 9x, Loved this program... its good, configurrable. you can
+        save blacklists and whitelists... but to be honest, they stopped
+        development and the open Source version i tried, .... kind of fell
+        flat.... no so much &quot;cause this program sucks&quot;, but
+        because popups have gotten more advanced to get around those old
+        blockers.<br/>
+  </li>  
+  <li><strong><a href="http://www.popupcop.com" target="_blank">Popup Cop (SW)</a></strong> -
+           here is one if you use IE (not sure about IE
+        clone) as your default browser, but it will not work in any other
+        browser . .. it has a
+        TOOOOOOOOOOOOOOOOON of options, and features,... i could probably
+        make a whole site (not page) just to describe it ... alot of the
+        previously mentioned &quot;smart popups&quot; can be detected and
+        removed without any user interaction... if you want a popup (say a
+        prompt on a site is being blocked... one you know you need to see),
+        just click enable popups on this site
+  </li>
+</ul>
+<p><a href="http://www.admuncher.com" target="_blank"><img src="../../../progIcons/security/ad_muncher.png" alt="Ad Muncher Icon"/></a>
+  <ul></ul>
+  <li><strong><a href="http://www.admuncher.com" target="_blank">Ad Muncher</a></strong>This works in all programs... from web browsers IE, Opera, Mozilla, Firefox, (any IE clone), Paltalk... 
+    and although i have not tried it this way, Yahoo messanger and adware programs.... This is a GREAT TOOL, 
+    that although I love Popup Cop... I really do... this is just a notch or two above ... This blocks alot like
+    Popup Cop.. it doesn&apos;t need alot of user interaction, but if you need to set it up so it doesn&apos;t block on 
+    a certain site, its a piece of  cake to do...
+  </li>
+</p>
+<p>
+  Many non-IE (thats counting IE clones) have built in popup blockers that do very well .. I do not have the
+  URL at the moment, but several sites put 3 sup rd party popup blockers up against some of the
+   built in blockers and the built in blockers rated much higher. All of these (if you have one that doesn&apos;t ...
+  you do not have a good one) have exception lists, so if
+   you find its blocking too much... just tell it not to block a particular item ... OR on a particular page
+</p>
+<p class="header">&quot;Non Internet Explorer Browsers &quot; that have great popup blockers</p>
+<ul>
+  <li><span class="boldUnderline">Internet Explorer Clones</span>
+    <ul>
+      <li><span class="boldUnderline"><a href="http://www.avant.com">Avant</a></span> - Not to be outdone ... this is VERY simple to Maxthon ... but yet different in some ways ...
+         try both and see for yourself
+      </li>
     </ul>
-    </div>
- 
-    EOD
-;
-?>
-<?php include $_SERVER['DOCUMENT_ROOT'].("/templates/windows/baseWinSecurityTemplate.php"); ?>   
-    
-    
+  </li><span><a href="http://www.firefox.com" target="_blank"><img src="../../progIcons/browsers/firefox_1.png" alt="Firefox Icon" width="36" height="36"/> Firefox</a></span>      - This is the biggest (IMHO) alternative to IE right now ... not counting when I am testing my site in
+      other browsers, this is the one I use all the time for regular use.
+</ul>
