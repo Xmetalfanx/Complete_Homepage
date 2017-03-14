d68c4d19987bde21271318c93aeb1a0c13ebbaee
Xmetalfanx <Xmetalfanx@yahoo.com>
2017-03-14 02:20:10 -0400

    getting main page Bourbon and Neat figured out

--- index.php
+++ index.php
@@ -3,51 +3,31 @@
   <head>
     <meta charset="utf-8">
     <title> Xmetal&apos;s Homepage</title>
+    <script src="http://code.jquery.com/jquery-3.1.1.min.js" type="text/javascript"></script>
+    <script scr="/scripts/nav.js" type="text/javascript"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
     <link href="/stylesheets/layout.css" rel="stylesheet" type="text/css" media="all">
   </head>
   <body>
 
-    <header role="banner" class="navigation">
-      <div class="navigation-wrapper">
-        <a id="js-mobile-menu" href="javascript:void(0)" class="navigation-menu-button">Xmetal's Homepage</a>
-        <nav role="navigation">TEST
-          <ul id="js-navigation-menu" class="navigation-menu show">
-            <li class="nav-link"><a href="/linuxHQ/index.php">LinuxHQ </a></li>
-            <li class="nav-link"><a href="/techHQ/index.php">TechHQ</a></li>
-          </ul>
-        </nav>
-      </div>
-    </header>
-
-    <div class="container">
-      <div class="row">
-        <div class="card hidden-md-down">
-          <div class="card-block">
-            <h4 class="card-header">R.I.P. Midfngr            |     </h4>
-            <p class="card-text">
-              <p>Please visit</p><img src="/graphics/midfngr.jpg" alt="midfngr thumbnail" class="show-for-medium">
-              <a href="https://www.youtube.com/user/midfingr/undefined" target="_blank" class="show-for-medium"> Midfngr's Youtube Channel</a>
-            </p>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-block">
-            <h3 class="card-header">ATTENTION!</h3>
-            <p class="card-text">
-              <p>Most of the site is broken at the moment ... I have a big idea on my side of things how to redo a few items to make it easier to manage and fix a few things that have been annoying me ... bottom line is I KNOW ITS BROKEN</p>
-            </p>
-          </div>
-        </div>
-        <div class="card">
-          <div class="card-block">
-            <p class="card-text">
-              <p>Feb 7 2017 Update - I have been working on the templates and converting stuff over so i can then just go into the individual pages and apply the new way I am doing things all at once.  Think of it like before I get all the furnature in and setup just right ... I have to get the framework of the house setup the way I want/need first.  </p>
-            </p>
-          </div>
-        </div>
+    <?php include 'modules/frontnav.php'; ?>
+    <div class="hero">
+      <div class="hero-content">
+        <p>Please visit</p><img src="/graphics/midfngr.jpg" alt="midfngr thumbnail" class="show-for-medium"><a href="https://www.youtube.com/user/midfingr/undefined" target="_blank" class="show-for-medium"> Midfngr's Youtube Channel</a>
       </div>
     </div>
+    <ul class="bullets">
+      <li class="bullet">
+        <div class="bullet-content">
+          <p>Most of the site is broken at the moment ... I have a big idea on my side of things how to redo a few items to make it easier to manage and fix a few things that have been annoying me ... bottom line is I KNOW ITS BROKEN</p>
+        </div>
+      </li>
+      <li class="bullet">
+        <div class="bullet-content">
+          <p>Feb 7 2017 Update - I have been working on the templates and converting stuff over so i can then just go into the individual pages and apply the new way I am doing things all at once.  Think of it like before I get all the furnature in and setup just right ... I have to get the framework of the house setup the way I want/need first.  </p>
+        </div>
+      </li>
+    </ul><?php include 'modules/frontFooter.tpl'; ?>
   </body>
-</html><?php include $_SERVER['DOCUMENT_ROOT'] . 'modules/frontFooter.php'; ?>
+</html>
