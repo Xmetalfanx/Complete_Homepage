5b636e66ac078bfda19c16a91c4e66c668144c2f
Xmetalfanx <Xmetalfanx@yahoo.com>
2017-06-01 19:56:18 -0400

    Added and Re-ordered Distros in Distro Section's Menu
    Signed-off-by: Xmetalfanx <Xmetalfanx@yahoo.com>

--- index.php
+++ index.php
@@ -4,15 +4,23 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title> Xmetal&apos;s Homepage</title>
+
+
     <!-- For Navigation Menu from Bourbon.io and it's Refills section   -->
-    <script src="http://xmetal.x10.mx/scripts/nav.js"></script>
+    <script src="http://xmetal.x10.mx/scripts/nav.js" type="text/javascript"></script>
+
     <!-- JQuery-->
     <script src="http://xmetal.x10.mx/scripts/jquery-3.2.0.min.js"></script>
+
+
     <!-- Tether-->
     <script src="http://xmetal.x10.mx/tether133/js/tether.min.js"></script>
+
     <!-- Bootstrap 4 alpha 6 -->
-    <link rel="stylesheet" href="http://xmetal.x10.mx/bootstrap/css/bootstrap.min.css">
-    <script src="http://xmetal.x10.mx/bootstrap/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="http://xmetal.x10.mx/frameworks/bootstrap/css/bootstrap.min.css">
+    <script src="http://xmetal.x10.mx/frameworks/bootstrap/js/bootstrap.min.js"></script>
+
+
     <link href="/stylesheets/customCSS.css" rel="stylesheet" type="text/css" media="all">
   </head>
   <body>
@@ -20,7 +28,10 @@
     <?php include 'modules/frontnav.php'; ?>
     <div class="hero">
       <div class="hero-content">
-        <p>Please visit</p><img src="/graphics/midfngr.jpg" alt="midfngr thumbnail" class="show-for-medium">
+        <p>Please visit</p>
+
+        <img src="/graphics/midfngr.jpg" alt="midfngr thumbnail" class="show-for-medium">
+
         <a href="https://www.youtube.com/user/midfingr/undefined" target="_blank" class="show-for-medium"> Midfngr's Youtube Channel</a>
       </div>
     </div>
@@ -35,9 +46,27 @@
           <p>Feb 7 2017 Update - I have been working on the templates and converting stuff over so i can then just go into the individual pages and apply the new way I am doing things all at once.  Think of it like before I get all the furnature in and setup just right ... I have to get the framework of the house setup the way I want/need first.  </p>
         </div>
       </li>
+
+      <li class="bullet">
+        <div class="bullet-content">
+          <h4>My Github Other Github project </h4>
+
+          <a href="https://github.com/Xmetalfanx/linuxSetup" target="_blank">
+            LinuxSetup</a>
+
+            - Helps a user setup some things with a menu system on Ubuntu, Fedora, OpenSuse, and (minor things for this distro ... so far) Solus.   This includes adding well known third party repos, enabling coming setings not set by default, and adding common software in one shot
+
+            <br /><br />
+
+            <a href="https://github.com/Xmetalfanx/linuxThemes" target="_blank">
+            LinuxThemeing</a>
+
+             This originally was going to be a part of the LinuxSetup scripts, though it grew too much and I decided to branch it out.  This installs themes and icon packs of well liked Linux Themes into your system.  I have alot of plans for this for version and distro detection.
+        </div>
+      </li>
     </ul>
+
+<?php include 'modules/footer.tpl'; ?>
+
   </body>
-
-<?php include $sitePath . "modules/footer.tpl"; ?>
-
 </html>
