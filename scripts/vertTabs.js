e0ace7c939fbfb0bd12a46ff4a0693b58ec2e577
Xmetalfanx <Xmetalfanx@yahoo.com>
2017-07-21 16:11:37 -0400

    Gave up on trying to get the vertical tabs to work on the Linux App Section
    replaced them with horizontal tabs which i know work on other parts of the site
    
    Also some MINOR CSS tweaks for mobile ... i see something is completely broken on mobile and thats whats I am going to fix next ...
    - things get all right aligned and only take 2 of the 12 columns ... going to troubleshoot that

--- scripts/vertTabs.js
+++ scripts/vertTabs.js
@@ -1,13 +1,14 @@
 $(".js-vertical-tab-content").hide();
-$(".js-vertical-tab-content:first").show();
+$(".js-vertical-tab-content:first-of-type").show();
 
 /* if in tab mode */
 $(".js-vertical-tab").click(function(event) {
   event.preventDefault();
 
-  $(".js-vertical-tab-content").hide();
   var activeTab = $(this).attr("rel");
-  $("#"+activeTab).show();
+  $currentTab = $("#"+activeTab);
+  $currentTab.parent().find('.js-vertical-tab-content').hide();
+  $currentTab.show();
 
   $(".js-vertical-tab").removeClass("is-active");
   $(this).addClass("is-active");
@@ -20,13 +21,14 @@ $(".js-vertical-tab").click(function(event) {
 $(".js-vertical-tab-accordion-heading").click(function(event) {
   event.preventDefault();
 
-  $(".js-vertical-tab-content").hide();
-  var accordion_activeTab = $(this).attr("rel");
-  $("#"+accordion_activeTab).show();
+  var activeTab = $(this).attr("rel");
+  $currentTab = $("#"+activeTab);
+  $currentTab.parent().find('.js-vertical-tab-content').hide();
+  $currentTab.show();
 
   $(".js-vertical-tab-accordion-heading").removeClass("is-active");
   $(this).addClass("is-active");
 
   $(".js-vertical-tab").removeClass("is-active");
-  $(".js-vertical-tab[rel^='"+accordion_activeTab+"']").addClass("is-active");
+  $(".js-vertical-tab[rel^='"+accordionActiveTab+"']").addClass("is-active");
 });
