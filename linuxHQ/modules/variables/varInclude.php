35d8b4a6265fafc5f6acafcb50ec91f1d116bd1a
Xmetalfanx <Xmetalfanx@yahoo.com>
2017-08-24 22:12:04 -0400

    Removed Obsolete variable files

--- linuxHQ/modules/variables/varInclude.php
+++ linuxHQ/modules/variables/varInclude.php
@@ -1,7 +1,3 @@
 <?php
-  include $sitePath . '/linuxHQ/modules/variables/distroWatchVar.php';
-  include $sitePath . '/linuxHQ/modules/variables/myDistroPages.php';
-  include $sitePath . '/linuxHQ/modules/variables/sshots.php';
-  include $sitePath . '/linuxHQ/modules/variables/officalHP.php';
 
 ?>
