58737816b54548650450b856616916e0cc8ee56c
Xmetalfanx <Xmetalfanx@yahoo.com>
2017-09-29 15:54:26 -0400

    More work on Desktop frontpage

--- linuxHQ/desktops/desktopsIndex.php
+++ linuxHQ/desktops/desktopsIndex.php
@@ -8,164 +8,145 @@
 
 ?>
 
+<h3>Heavier Desktops </h3>
 
-<div id="LinuxSection">
-  <div class="card">
-    <div class="card-block">
-      <h4 class="card-title"><a href="https://solus-project.com/budgie/">Budgie</a></h4>
-      <div class="card-text hidden-xs hidden-sm">
-        <div class="screenshots">
-					<?php echo $budgieSS1 . $budgieSS2 . $budgieSS3; ?>
-				</div>
+<div class="row">
+  <div class="card" style="width: 33%;padding-right: 10px;">
+    <img class="card-img-top hidden-md-down" src="../screenshots/DE/suseGnome1.jpg" alt="OpenSuse Gnome" />
+    <div class="card-body">
+      <h4 class="card-title">Gnomeshell/Gnome 3</h4>
+      <p class="card-text">
+        <div class="font-weight-bold">Some distros that Feature:</div>
+         Ubuntu, Solus Gnome, OpenSuse, Arch/Antergos, Fedora 
+      </p>
 
-        <div class="font-weight-bold">Distros that feature:</div>Solus (Main distro), Budgie Remix (Budgie on Ubuntu base)
-        <div class="font-weight-bold">CPU speed:</div>
-        <div class="font-weight-bold">Memory for Comfortable USE:</div>
-        <div class="font-weight-bold">My Comments:</div>
-      </div>
+      <a href="#" class="btn btn-primary">
+          OpenSuse Homepage 
+      </a>
+
+      <a href="#" class="btn btn-primary">
+          Fedora Homepage 
+      </a>
+
+      <br /><br /> 
+      <a href="#" class="btn btn-primary">
+          Solus Gnome Homepage 
+      </a>
     </div>
   </div>
 
-  <div class="card">
-    <div class="card-block">
-      <h4 class="card-title">
-				<a href="http://developer.linuxmint.com/">Cinnamon</a></h4>
+  <div class="card" style="width: 33%;">
+    <img class="card-img-top hidden-md-down" src="../screenshots/ubuntu/mintOffCinn1.jpg" alt="Mint Cinnamon" />
+    <div class="card-body">
+      <h4 class="card-title">Cinnamon</h4>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 
-			<div class="card-text hidden-xs hidden-sm">
-        <div class="screenshots"><?php echo $mintCinSS1 . $fedoraCinnSS1; ?></div>
-        <div class="font-weight-bold">Distros that feature:</div>
-				<?php echo "Linux Mint" . $fedoraHOME . "Arch"; ?>
-      </div>
-
-			<div class="linuxContainer">Requirenments
-        <div class="font-weight-bold">CPU speed:</div>512MB
-        <div class="font-weight-bold">Memory:</div>1GHz
-        <div class="font-weight-bold">My Comments:</div>I would say a MINIMUM OF maybe in the 1.8GHz+ for the processor (guessing there) ... and at least 2GB of RAM for COMFORTABLE usage, myself
-      </div>
+      <a href="http://www.linuxmint.com" target="_blank" class="btn btn-primary">
+          Linux Mint Homepage 
+      </a>
     </div>
   </div>
 
-	<div class="card">
-    <div class="card-block">
-      <h4 class="card-title">
-				<a href="https://www.gnome.org/">Gnomeshell/Gnome 3</a></h4>
-      <div class="card-text">
-        <div class="screenshots"><?php echo $fedoraGnome_SS1 . $suseGnome_SS1; ?></div>
-
-				<div class="linuxContainer">
-          <div class="font-weight-bold">Distros that Feature:</div><?php echo $archHOME . $fedoraHOME . $suseHOME; ?>
-        </div>
-
-				<div class="linuxContainer">
-          <div class="font-weight-bold">CPU speed:</div>768MB
-          <div class="font-weight-bold">Memory for Comfortable USE:</div>400MHz
-          <div class="font-weight-bold">My Comments:</div>There is NO WAY I can respectfully agree with this...  My 2.2Ghz dualcore, 4GB Toshiba from 2009, would be an indicator OF THE MINIMUM I would even think of .. these specs have to be an UN-updated spec for Gnome2 but not Gnome3/Gnomeshell .. see "Mate" for the so called continuation of Gnome2, below
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="card">
-    <div class="card-block">
-      <h4 class="card-title"><a href="http://lxde.org/">LXDE/LXQT</a></h4>
-      <div class="card-text">
-        <div class="screenshots"><?php	echo $lxleSS1; ?></div>
-
-				<div class="linuxContainer">
-          <div class="font-weight-bold">Distros that Feature:</div><?php echo $lubuntuHOME . $LXLEHOME . $PepperMintHOME . $archHOME; ?>
-        </div>
-
-				<div class="linuxContainer">
-          <div class="font-weight-bold">CPU speed:</div>128MB
-          <div class="font-weight-bold">Memory:</div>266MHz
-          <div class="font-weight-bold">My Comments:</div>I am not 100% sure on these recommendations, though if there is one option listed for "lower end specs" that may be fine  with specs like these... LXDE/LXQT (note to self: put in link to explain LXDE-vs-LXQT here) would be the option that works good on lower end hardware.
-        </div>
-      </div>
-    </div>
-  </div>
-
-	<div class="card">
-    <div class="card-block">
-      <h4 class="card-title">
-				<a href="http://xmetal.x10.mx/linuxHQ/desktops/mate.php" target="_blank">Mate</a></h4>
-
-			<div class="card-text">
-        <p>Updated DE that some &#39;consider&#39; a continuation of &#39;Gnome 2&#39; and it’s look and feel</p>
-        <div class="screenshots">
-					<?php echo $mintMateSS1 . $mintMateSS2; ?>
-				</div>
-        <div >
-					<a href="http://mate-desktop.org/" target="_blank">Mate Homepage</a>
-
-					<div class="linuxContainer">
-            <div class="font-weight-bold">Distros that Feature:</div><?php echo $UMateHome . $fedoraHOME . $mintHOME; ?>
-          </div>
-
-					<div class="linuxContainer">
-            <div class="font-weight-bold">CPU speed:</div>512MB<br />
-            <div class="font-weight-bold">Memory:</div>800MHz<br />
-            <div class="font-weight-bold">My Comments:</div>I am not sure what I would say for "Comfortable" usage daily, though I have the Ubuntu Mate Pi2 version here, and while not perfect at times ... consider the cost of the Pi2 + accessories (for an example) ... it is REALLY not too bad
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-
-  <div class="card">
-    <div class="card-block">
-      <h4 class="card-title">
-				<a href="/linuxHQ/desktops/KDE.php">Plasma 5 (aka &apos;KDE 5&apos;)</a></h4>
-      <div class="card-text">
-
-				<div class="screenshots">
-					<?php echo $antergosKDE1 . $manjaroKDE5 . $kaosKDE_SS1; ?></div>
-          <div class="font-weight-bold">
-						<a href="https://www.kde.org/workspaces/plasmadesktop/" target="_blank"></a>Plasma 5 (aka &apos;KDE 5&apos;)
-            <div class="linuxContainer">
-							<span class="title">Distros that feature:</span>
-							<?php echo $antergosHOME, $archHOME, $suseHOME , $fedoraHOME , $kubuntuHOME , $mintHOME; ?></div>
-            <div class="linuxContainer">
-							<span class="font-weight-bold">CPU speed:</span>1GHz<br />
-							<span class="font-weight-bold">Memory:</span>615MB <br />
-							<span class="font-weight-bold">My Comments:</span>I am not sure if these are KDE4 or Plasma 5 specs listed above though I would say at LEAST 2GB of RAM and 1.7GHZ+ for the processor.</div>
-          </div>
-
-      </div>
-    </div>
-  </div>
-
-
-  <div class="card">
-    <div class="card-block">
-      <h4 class="card-title"><a href="http://www.xfce.org/">XFCE</a></h4>
-      <div class="card-text">
-        <div class="screenshots"><?php echo $manjaroXFCE1 . $xubuntuSS1; ?></div>
-          <div class="linuxContainer">
-            <div class="font-weight-bold">
-              Distros that feature:
-              <?php $mintHOME . $xubuntuHOME . $fedoraHOME . $suseHOME; ?>
-            </div>
-          </div>
-
-          <div class="linuxContainer">
-            <div class="font-weight-bold">CPU speed:</div>192 MB
-            <div class="font-weight-bold">Memory for Comfortable USE:</div>300Mhz
-            <span class="title">My Comments:</span>I'd say it may run at those specs above, though my thinking is at least 512MB of RAM to 768MB MINIMUM and maybe oh ...  1GHz for "regular" usage of what you'd probably want to do
-          </div>
-
-      </div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-block">
-      <div class="card-text">
-		  <a href="https://renewablepcs.wordpress.com/about-linux/kde-gnome-or-xfce/?_utm_source=1-2-2" target="_blank">
-			  GOOD SITE with default app info and spec information about Desktop Envirnoments.. much of the info the specs (not counting my thoughts) are from this site ... all credit goes to this website, linked here</a></div>
+  <div class="card" style="width: 33%;">
+    <img class="card-img-top hidden-md-down" src="../screenshots/ubuntu/kdeneon.png" alt="Card image cap">
+    <div class="card-body">
+      <h4 class="card-title">Plasma 5/KDE</h4>
+      <p class="card-text">Some Distros that feature: KDE Neon, Kubuntu,  Arch, OpenSuse, Fedora/Korora</p>
+       <a href="https://neon.kde.org/" target="_blank" class="btn btn-primary">
+          KDE Neon Homepage 
+      </a>
+      <a href="https://kubuntu.org/" target="_blank" class="btn btn-primary">
+          Kubuntu Homepage 
+      </a>
+      <br /><br />
+       <a href="https://kaosx.us/" target="_blank" class="btn btn-primary">
+          KaOS Homepage 
+      </a>
     </div>
   </div>
 </div>
 
+<br /><br />
+
+<div class="row">
+
+  <div class="card" style="width: 33%;">
+    <img class="card-img-top hidden-md-down" src="../screenshots/solus/budgie1.jpg" alt="Ubuntu Mate Screenshot" />
+    <div class="card-body">
+      <h4 class="card-title">Budgie</h4>
+      <p class="card-text">
+        <div class="font-weight-bold">Some Distros that Feature:</div>
+        Solus  
+      </p>
+
+      <a href="https://solus-project.com/download/" target="_blank" class="btn btn-primary">
+          Solus Homepage 
+      </a>
+
+    </div>
+  </div>
 </div>
+
+<hr />
+
+
+
+<h3> Lighter Desktops</h3>
+<p> Better for older systems or systems with lower-end specs </p><br />
+<div class="row">
+
+  <div class="card" style="width: 33%;">
+    <img class="card-img-top hidden-md-down" src="../screenshots/ubuntu/umate1.png" alt="Ubuntu Mate Screenshot" />
+    <div class="card-body">
+      <h4 class="card-title">Mate</h4>
+      <p class="card-text">
+        Updated DE that some &#39;consider&#39; a continuation of &#39;Gnome 2&#39; and it’s look and feel
+        <div class="font-weight-bold">Some Distros that Feature:</div>
+        Linux Mint, Ubuntu Mate, Solus Mate  
+      </p>
+
+      <a href="http://ubuntu-mate.org/" target="_blank" class="btn btn-primary">
+          Ubuntu Mate Homepage 
+      </a>
+
+      <a href="https://solus-project.com/download/" target="_blank" class="btn btn-primary">
+          Solus Mate Homepage 
+      </a>
+    </div>
+  </div>
+
+  <div class="card" style="width: 33%;">
+    <img class="card-img-top hidden-md-down" src="../screenshots/ubuntu/xubuntu1.png" alt="Ubuntu Mate screenshot" />
+    <div class="card-body">
+      <h4 class="card-title">XFCE</h4>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" target="_blank" class="btn btn-primary">Xubuntu Homepage</a>
+      <a href="https://manjaro.org/" target="_blank" class="btn btn-primary">
+          Manjaro Homepage 
+      </a>
+    </div>
+  </div>
+
+  <div class="card" style="width: 33%;">
+    <img class="card-img-top" src="../screenshots/ubuntu/lubuntu1.png" alt="Card image cap" />
+    <div class="card-body">
+      <h4 class="card-title">LXDE</h4>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      
+      <a href="http://lubuntu.net/" target="_blank" class="btn btn-primary">Lubuntu Homepage</a>
+      <a href="https://peppermintos.com/about/" target="_blank" class="btn btn-primary">Peppermint Homepage</a>
+    
+    </div>
+
+  </div>
+</div>
+
+<br /><hr />
+
+
+<br /> 
+<a href="https://renewablepcs.wordpress.com/about-linux/kde-gnome-or-xfce/?_utm_source=1-2-2" target="_blank">
+  GOOD SITE with default app info and spec information about Desktop Envirnoments.. much of the info the specs (not counting my thoughts) are from this site ... all credit goes to this website, linked here</a>
+ 
 
 <?php include $sitePath . "/modules/footer.tpl"; ?>
