0e28faa8fcf1e76254aa31dff9edaf04536fdf87
Xmetalfanx <Xmetalfanx@yahoo.com>
2018-04-26 00:15:16 -0400

    Adjusting frontpage
    - I am moving stuff away from Bourbon Refills ( https://refills.bourbon.io/ ) and going with similar things in the now Final (not a beta anymore) Bootstrap 4
    
    - The reason is that updates to Bourbon and Neat are breaking Refills and the team says Refills are no longer being updates to reflect that

--- index.php
+++ index.php
@@ -1,70 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title> Xmetal&apos;s Homepage</title>
-    <!-- JQuery-->
-    <script src="http://xmetal.x10.mx/scripts/jquery-3.2.1.min.js"></script>
-    <!-- For Navigation Menu from Bourbon.io and it's Refills section-->
-    <script src="http://xmetal.x10.mx/scripts/nav.js" type="text/javascript"></script>
-    <!-- Tether-->
-    <script src="http://xmetal.x10.mx/frameworks/tether133/js/tether.min.js"></script>
-    <!-- Bootstrap 4 alpha 6-->
-    <link rel="stylesheet" href="http://xmetal.x10.mx/frameworks/bootstrap/css/bootstrap.min.css">
-    <script src="http://xmetal.x10.mx/frameworks/bootstrap/js/bootstrap.min.js"></script>
-    <link href="/universalCSS/styling.css" rel="stylesheet" type="text/css" media="all"> </head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title> Xmetal&apos;s Homepage</title>
+  <!-- JQuery-->
+  <script src="http://xmetal.x10.mx/scripts/jquery.min.js"></script>
+  <!-- For Navigation Menu from Bourbon.io and it's Refills section-->
+  <script src="http://xmetal.x10.mx/scripts/nav.js" type="text/javascript"></script>
+  <!-- Tether-->
+  <script src="http://xmetal.x10.mx/frameworks/tether/js/tether.min.js"></script>
+  <!-- Bootstrap-->
+  <link rel="stylesheet" href="http://xmetal.x10.mx/frameworks/bootstrap/css/bootstrap.min.css">
+  <script src="http://xmetal.x10.mx/frameworks/bootstrap/js/bootstrap.min.js"></script>
+  <!-- Universal CSS file-->
+  <link href="/universalCSS/styling.css" rel="stylesheet" type="text/css" media="all">
+</head>
+  <body>
 
-<body>
-  
-<div id="container">
-    <header class="navigation">
-      <div class="navigation-wrapper">
-        <a href="http://xmetal.x10.mx" class="logo hidden-md-down">Xmetal's Homepage</a>
+    <?php include 'modules/frontnav.tpl'; ?>
 
-        <a id="js-mobile-menu" href="http://xmetal.x10.mx" class="navigation-menu-button">Xmetal's Homepage</a>
-        <nav>
-          <ul id="js-navigation-menu" class="navigation-menu show">
-            <li class="nav-link"><a href="/linuxHQ/index.php">Linux HQ</a></li>
-            <li class="nav-link"><a href="/techHQ/index.php">Tech HQ</a></li>
-          </ul>
-        </nav>
-      </div>
-    </header>
-
-
-
-    <div class="hero">
-        <div class="hero-content">
-           <p>Please visit</p>
-           <img src="/graphics/midfngr.jpg" alt="midfngr thumbnail" class="show-for-medium">
-           <a href="https://www.youtube.com/user/midfingr/undefined" target="_blank" class="show-for-medium"> Midfngr's Youtube Channel</a>
-       </div>
+    <div class="coverImage">
     </div>
 
+  <div class="row clearfix">
+    <div class="col-lg-6">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+    <div class="col-lg-6">
+      <h4>My Github Other Github project</h4><a href="https://github.com/Xmetalfanx/linuxSetup" target="_blank">
+        LinuxSetup</a>
+        - Helps a user setup some things with a menu system on Ubuntu, Fedora, OpenSuse, and (minor things for this distro ... so far) Solus.   This includes adding well known third party repos, enabling coming setings not set by default, and adding common software in one shot<br><br>
 
-    <ul class="bullets">
-        <li class="bullet">
-            <div class="bullet-content">
-                <p>July 20, 2017 - Site under construction... I hope to make things look alot better on mobile devices .. this is part of the reason for the big change from alot of custom CSS Styles to "Bourbon.io" and Bootstrap (though Foundation looks like a good Bootstrap alternative)</p>
-            </div>
-        </li>
-        <li class="bullet">
-            <div class="bullet-content">
-                <h4>My Github Other Github project </h4> <a href="https://github.com/Xmetalfanx/linuxSetup" target="_blank">
-        LinuxSetup</a> - Helps a user setup some things with a menu system on Ubuntu, Fedora, OpenSuse, and (minor things for this distro ... so far) Solus. This includes adding well known third party repos, enabling coming setings not set by default, and adding common software in one shot
-                <br />
-                <br />
-                <a href="https://github.com/Xmetalfanx/linuxThemes" target="_blank">
-        LinuxThemeing</a> This originally was going to be a part of the LinuxSetup scripts, though it grew too much and I decided to branch it out. This installs themes and icon packs of well liked Linux Themes into your system. I have alot of plans for this for version and distro detection. </div>
-        </li>
-    </ul>
-
-    <?php include 'modules/footer.tpl'; ?>
-
-</div> <!-- Ends Container DIV -->
-
+        <a href="https://github.com/Xmetalfanx/linuxThemes" target="_blank">
+        LinuxTheming</a>
+         This originally was going to be a part of the LinuxSetup scripts, though it grew too much and I decided to branch it out.
+        This installs themes and icon packs of well liked Linux Themes into your system.  I have alot of plans for this for version and distro detection.
+    </div>
+  </div>
+  <?php include 'modules/footer.tpl'; ?>
 </body>
 
 </html>
--- modules/footer.tpl
+++ modules/footer.tpl
@@ -2,36 +2,27 @@
       </div> <!-- Ends Second Column opened in template files  -->
 </div> <!-- Ends Row opened in the template file, if i recall -->
 
+<footer class="row clearfix">
+  <div class="col-lg-6">
+    <div>Validation graphics go here, later</div>
+  </div>
+  <div class="col-lg-6">
+    <span>Xmetal/Xmetalfanx 2013-*</span>
+    <div class="footer-social">
+      <a href="https://twitter.com/Xmetalfanx" target="_blank">
+        <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/twitter-logo-circle.png" alt="Twitter"/></a>
 
-<footer class="footer-2">
-  <ul>
-    <li>Validation Gif Here</li>
-  </ul>
-
-
-  <div class="footer-secondary-links">
-    <ul>
-      <li><a href="javascript:void(0)">Xmetal/Xmetalfanx 2013-*</a></li>
-    </ul>
-    <ul class="footer-social">
-      <li>
-        <a href="https://twitter.com/Xmetalfanx" target="_blank">
-          <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/twitter-logo-circle.png" alt="Twitter"/>
-        </a>
-      </li>
-      <li>
-        <a href="javascript:void(0)">
+        <a href="#">
           <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/youtube-logo-circle.png" alt="YouTube"/>
         </a>
-      </li>
-      <li>
+
         <a href="https://plus.google.com/+Xmetalfanx2014/about" target="_blank">
-          <img src="http://xmetal.x10.mx/graphics/gplusfooter.png" alt="Google Plus" />
-        </a>
-      </li>
-    </ul>
+          <img src="http://xmetal.x10.mx/graphics/gplusfooter.png" alt="Google Plus"/>
+      </a>
+      </div>
   </div>
-  </footer>
+</footer>
+
 </div> <!-- Ends Container that is opened up in the header file -->
 
 </body>
