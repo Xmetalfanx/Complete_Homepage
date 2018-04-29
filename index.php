6f9ac4bd055d0d8cca2e22638c78ffaa91db4289
Xmetalfanx <Xmetalfanx@yahoo.com>
2018-04-29 15:18:09 -0400

    Updated Normalizer from 7.0 to 8.0
    - removed un-needed (I hope) CSS

--- index.php
+++ index.php
@@ -4,39 +4,40 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title> Xmetal&apos;s Homepage</title>
-  <!-- JQuery-->
-  <script src="http://xmetal.x10.mx/scripts/jquery.min.js"></script>
+
+  <!-- JQuery and Popper -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+
   <!-- For Navigation Menu from Bourbon.io and it's Refills section-->
   <script src="http://xmetal.x10.mx/scripts/nav.js" type="text/javascript"></script>
+
   <!-- Tether-->
   <script src="http://xmetal.x10.mx/frameworks/tether/js/tether.min.js"></script>
-  <!-- Bootstrap-->
-  <link rel="stylesheet" href="http://xmetal.x10.mx/frameworks/bootstrap/css/bootstrap.min.css">
-  <script src="http://xmetal.x10.mx/frameworks/bootstrap/js/bootstrap.min.js"></script>
+
+  <!-- Bootstrap CDN -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
   <!-- Universal CSS file-->
   <link href="/universalCSS/styling.css" rel="stylesheet" type="text/css" media="all">
 </head>
-  <body>
+<body>
 
-    <?php include 'modules/frontnav.tpl'; ?>
+  <?php include 'modules/frontnav.tpl'; ?>
+  <div class="coverImage"></div>
+  <div id="mainText" class="row">
 
-    <div class="coverImage">
-    </div>
-
-  <div class="row clearfix">
     <div class="col-lg-6">
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
-    <div class="col-lg-6">
-      <h4>My Github Other Github project</h4><a href="https://github.com/Xmetalfanx/linuxSetup" target="_blank">
-        LinuxSetup</a>
-        - Helps a user setup some things with a menu system on Ubuntu, Fedora, OpenSuse, and (minor things for this distro ... so far) Solus.   This includes adding well known third party repos, enabling coming setings not set by default, and adding common software in one shot<br><br>
 
-        <a href="https://github.com/Xmetalfanx/linuxThemes" target="_blank">
-        LinuxTheming</a>
-         This originally was going to be a part of the LinuxSetup scripts, though it grew too much and I decided to branch it out.
-        This installs themes and icon packs of well liked Linux Themes into your system.  I have alot of plans for this for version and distro detection.
-    </div>
+    <div class="col-lg-6">
+    <h4>My Github Other Github project</h4>
+    <a href="https://github.com/Xmetalfanx/linuxSetup" target="_blank">LinuxSetup</a>- Helps a user setup some things with a menu system on Ubuntu, Fedora, OpenSuse, and (minor things for this distro ... so far) Solus.   This includes adding well known third party repos, enabling coming setings not set by default, and adding common software in one shot<br><br>
+    <a href="https://github.com/Xmetalfanx/linuxThemes" target="_blank">LinuxThemeing</a> This originally was going to be a part of the LinuxSetup scripts, though it grew too much and I decided to branch it out.
+    This installs themes and icon packs of well liked Linux Themes into your system.  I have alot of plans for this for version and distro detection.
+  </div>
   </div>
   <?php include 'modules/footer.tpl'; ?>
 </body>
--- modules/linux/linuxBaseHeader.tpl
+++ modules/linux/linuxBaseHeader.tpl
@@ -17,21 +17,24 @@
   <link rel="index" href="/index.php">
 
 
-    <!-- JQuery-->
-    <script src="/scripts/jquery.min.js"></script>
+  <!-- JQuery and Popper -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
 
     <!-- For Navigation Menu from Bourbon.io and it's Refills section   -->
-    <script src="/scripts/nav.js" type="text/javascript"></script>
 
-    <script src="/scripts/tabRefills.js" type="text/javascript"></script>
+    <!-- <script src="/scripts/tabRefills.js" type="text/javascript"></script>
+    -->
+
     <script src="/scripts/vertTabs.js" type="text/javascript"></script>
+
 
     <!-- Tether-->
     <script src="/frameworks/tether/js/tether.min.js"></script>
 
-    <!-- Bootstrap 4 alpha 6 -->
-    <link rel="stylesheet" href="/frameworks/bootstrap/css/bootstrap.min.css">
-    <script src="/frameworks/bootstrap/js/bootstrap.min.js"></script>
+    <!-- Bootstrap CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
 
     <!-- Universal CSS file-->
     <link href="/universalCSS/styling.css" rel="stylesheet" type="text/css" media="all">
