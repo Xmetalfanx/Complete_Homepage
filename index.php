f9eb33d027d6100f816cadc3fd2abbc2001eaff9
Xmetalfanx <Xmetalfanx@yahoo.com>
2018-05-04 03:20:40 -0400

    Removed local framwework code and scripts, for CDNs

--- index.php
+++ index.php
@@ -9,11 +9,9 @@
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
 
-  <!-- For Navigation Menu from Bourbon.io and it's Refills section-->
-  <script src="http://xmetal.x10.mx/scripts/nav.js" type="text/javascript"></script>
-
-  <!-- Tether-->
+  <!-- Tether
   <script src="http://xmetal.x10.mx/frameworks/tether/js/tether.min.js"></script>
+  -->
 
   <!-- Bootstrap CDN -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
