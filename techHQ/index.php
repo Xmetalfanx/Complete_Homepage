95ea3f72a94c12380d54b5a8294b571d2e96737c
Xmetalfanx <Xmetalfanx@yahoo.com>
2018-05-08 05:10:51 -0400

    re-arranged some SCSS files

--- techHQ/index.php
+++ techHQ/index.php
@@ -8,7 +8,8 @@
 ?>
 
 
-<div class="hidden-sm-down">Educational Sites</div>
+<div class="hidden-sm-down">
+  <h3>Educational Sites</h3>
 
   <div class="row">
     <div class="col-md-3 col-lg-3 col-xl-3">
@@ -21,14 +22,18 @@
     </div>
     <div class="col-md-3 col-lg-3 col-xl-3">
       <a href="https://www.digitalocean.com/community/" target="_blank">
-        <img src="/techHQ/graphics/digital-ocean.png" alt="Digital Ocean Graphic"/></a>
+        <img
+         src="/techHQ/graphics/digital-ocean.png" alt="Digital Ocean Graphic"/></a>
     </div>
   </div>
   <!-- Ends Row--><br/><br/>
   <div class="row">
-    <div class="col-md-9 col-lg-9 col-xl-9"><a href="http://www.kristakingmath.com/" target="_blank">Krista King Math                </a><img src="/techHQ/graphics/kking.png" alt="Krista King Math Site graphics"/><br/><a href="https://www.youtube.com/channel/UCUDlvPp1MlnegYXOXzj7DEQ">(or Her Channel on Youtube)</a></div>
+    <div class="col-md-9 col-lg-9 col-xl-9">
+      <a href="http://www.kristakingmath.com/" target="_blank">Krista King Math </a>
+      <img src="/techHQ/graphics/kking.png" alt="Krista King Math Site graphics"/><br/>
+      <a href="https://www.youtube.com/channel/UCUDlvPp1MlnegYXOXzj7DEQ">(or Her Channel on Youtube)</a></div>
   </div>
-  <!-- Ends Row-->
+  <!-- Ends Row--></div>
 
   <div class="hidden-sm-up">
     <p>Content not intended to be seen on a screen this small</p>
