6adb7ffaf20e3ca7c53aa02a9f73a3618d01e28e
Xmetalfanx <Xmetalfanx@yahoo.com>
2018-11-12 20:37:38 -0500

    Added page for Anti-X

---
+++ linuxHQ/distros/debian/antix.php
@@ -0,0 +1,12 @@
+<?php
+
+	$sitePath = "/home/xmetalx1/public_html";
+	include $sitePath . "/templates/linux/baseLinuxDistro.tpl";
+
+	$pageTitle = 'Anti-X';
+
+	echo '<script type="text/javascript">';
+		echo 'var localDistroName = "antix"; ';
+	echo '</script>';
+
+?>
