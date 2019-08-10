99802d1294cec0eeffd1686377f87d12f55d4cf6
Xmetalfanx <Xmetalfanx@yahoo.com>
2019-08-10 19:48:29 -0400

    Working Tests

--- techHQ/modules/techSoftwareCol1.tpl
+++ techHQ/modules/techSoftwareCol1.tpl
@@ -7,14 +7,20 @@
     <div class="card-header bg-primary text-white"><a href="/techHQ/software/internetRelated.php">Internet Related</a></div>
   </div>
   <div class="card">
-    <div class="card-header bg-primary text-white">Multimedia Related</div><a href="/techHQ/software/multimedia/audio.php">Audio</a><br/><a href="/techHQ/software/multimedia/graphics.php">Graphics</a><br/><a href="/techHQ/software/multimedia/media_players_audio.php">Media Players <em>Audio</em><br/></a><a href="/techHQ/software/multimedia/media_players_video.php">Media Players <em>Video </em><br/></a><a href="/techHQ/software/multimedia/videoPrograms">Video Related Programs</a><br/>
+    <div class="card-header bg-primary text-white">Multimedia Related</div>
+      <a href="/techHQ/software/multimedia/audio.php">Audio</a><br/>
+      <a href="/techHQ/software/multimedia/graphics.php">Graphics</a><br/>
+      <a href="/techHQ/software/multimedia/media_players_audio.php">Media Players <em>Audio</em><br/></a>
+      <a href="/techHQ/software/multimedia/media_players_video.php">Media Players <em>Video </em><br/></a>
+      <a href="/techHQ/software/multimedia/videoPrograms">Video Related Programs</a><br/>
   </div>
   <div class="card">
     <div class="card-header bg-primary text-white"> <a href="/techHQ/software/productivity.php">Office/Productivity Programs</a></div>
   </div>
   <div class="card">
     <div class="card-header bg-primary text-white">
-      <a href="/techHQ/software/securityPrograms.php">Security Related</a></div><br/>
+      <a href="/techHQ/software/securityPrograms.php">Security Related</a>
+    </div><br/>
 
     <a href="/techHQ/software/security/virusANDMalwareScanners.php">Antivirus and Antimalware</a><br/>
     <a href="/techHQ/software/security/cleaners.php">System Cleaners</a><br/>
@@ -23,29 +29,39 @@
 
   <div class="card">
     <div class="card-header">
-      <a href="/techHQ/software/systemTools.php">System Tools</a></div><br/>
+      <a href="/techHQ/software/systemTools.php">System Tools</a>
+    </div><br/>
+      
+      
       <a href="/techHQ/software/sTools/archivers.php">Archivers</a><br/>
       <a href="/techHQ/software/sTools/backupSoftware.php">Backup Software</a><br/>
       <a href="/techHQ/software/sTools/burners.php">CD/DVD/Blue Ray Burners</a><br/>
       <a href="/techHQ/software/defrag.php">Defragmenters</a><br/>[File Managers]<br/>
       <a href="/techHQ/software/sTools/registryTools.php">Registry Tools</a><br/>
+
       <a href="/techHQ/software/sTools/startupManangers.php">Startup Managers</a><br/>
       <a href="/techHQ/software/sTools/sysInternals">SysInternels</a><br/>
+
       <a href="/techHQ/software/sTools/taskManagers.php">Task Manager</a><br/>
   </div>
 </div>
 
 <div class="card">
   <div class="card-header bg-primary text-white">External Links </div><br/>
-  <a href="http://www.majorgeek.com" target="_blank">
-    <img src="/techHQ/graphics/portals/mg_logo_trns.gif" alt="MajorGeek.com Link"/></a><br/>
+    <a href="http://www.majorgeeks.com" target="_blank">
+      <img src="/techHQ/graphics/portals/mg_logo_trns.gif" alt="MajorGeek.com Link"/>
+    </a><br/>
 
-  <a href="http://www.snapfiles.com"><img src="/techHQ/graphics/portals/snapfiles_freedlani.gif" alt="Snapfiles" style="width: 88px; height: 32px;" class="centerImage"/></a><br/>
+    <a href="http://www.snapfiles.com">
+      <img src="/techHQ/graphics/portals/snapfiles_freedlani.gif" alt="Snapfiles" class="my-2" />
+    </a><br/>
 
-  <a href="http://www.softpedia.com" target="_blank">
-    <img src="/techHQ/graphics/portals/softpedia_logo.gif" alt="Soft Pedia" style="width: 175px; height: 32px;" class="centerImage"/></a><br/>
+    <a href="http://www.softpedia.com" target="_blank">
+      <img src="/techHQ/graphics/portals/softpedia_logo.gif" alt="Soft Pedia" class="my-2" />
+    </a><br/>
 
     <a href="http://www.neowin.net/">NeoWin</a><br/>
 
     <a href="http://www.nonags.com/">NoNags</a><br/>
+
 </div>
--- techHQ/scripts/software.js
+++ techHQ/scripts/software.js
@@ -5,59 +5,95 @@ let softwareOutput = document.getElementById ('softwareSection');
 // "main" function
 
 // Where is "data" coming from?
-function softwareData(data) {
-  $(document).ready(function () {
+function softwareData (data) {
+  $ (document).ready (function () {
     const softwareJSONUrl = 'http://xmetal.x10.mx/techHQ/json/software.json';
 
     // Get JSON Data
     // note: this IS jquery and if Bootstrap 5 removes the need for Jquery, i may remove it too ... i'd need another way at that point
     $.getJSON (softwareJSONUrl, function (data) {
-
       // pick better var than "i", later
       for (let i in data) {
+        let mainCategories = Object.keys (data);
 
-        let mainCategories = Object.keys(data);
+        let subCategoriesList = Object.values (data);
 
-        let subCategoriesList = Object.values(data);
+        /* *** Var names are an issue ... they are confusing as hell the way I have it here */
 
-        // console.log("mainCategories: " + mainCategories);
+        for (let items in subCategoriesList) {
+          let subCatList = subCategoriesList[items];
 
-        // Shows: main Cats in a list
-        // softwareOutput.insertAdjacentHTML("beforeend","<div class=\"mb-2 font-weight-bold\">" + mainCategories + "</div>");
+          for (let eachSubCat in subCatList) {
+            let eachApp = subCatList[eachSubCat];
 
-        // *** Var names are an issue ... they are confusing as hell the way I have it here
+            softwareOutput.insertAdjacentHTML (
+              'beforeend',
+              '<div class="text-primary">' + eachSubCat + '</div>'
+            );
 
-          for(let items in subCategoriesList){
+            for (let apps in eachApp) {
+              const individualApps = eachApp[apps];
+              const moreInfo = individualApps.moreInfo;
 
-            let subCatList = subCategoriesList[items];
+              let progTitle = individualApps.progTitle;
+              let progName = individualApps.progName;
 
-            for(let eachSubCat in subCatList){
+              let progIcon = individualApps.images.icon;
+              let screenshot = individualApps.images.sshotURL;
 
-              let eachApp = subCatList[eachSubCat];
-              let appString = JSON.stringify(eachApp);
+              let description = moreInfo.progDesc;
+              let homepage = moreInfo.progHomepage;
+              let platform = moreInfo.progPlatform;
+              let worksOnWine = moreInfo.worksonwine;
 
-              console.log("eachSubCat: " + eachSubCat);
-              //console.log("appString: " + appString);
+              const iconDisplay = `<img src=\"${progIcon}\" alt=\"foobar alt atm\" style=\"height: 32px; width; \" \>
+                `;
 
-              for(let apps in eachApp){
+              const titleDisplay = `
+                <div class=\"card-header text-white py-1\"> ${iconDisplay} 
+                  <a href=\"${homepage}\" target="_blank">
+                  ${progTitle}
+                  </a>
+                </div>`;
 
-                let appTitle = eachApp[apps].progTitle;
-                console.log("appTitle:" + appTitle);
+              const wineDisplay = `
+                <div class=\" \">Works on Wine: 
+                  ${worksOnWine} 
+                </div>
+              `;
 
-                // console.log("eachApp[apps]: " + JSON.stringify(eachApp[apps]));
+
+              switch (platform)
+              {
+                case 'w':
+                    //console.log("Windows");
+
+                    platformIcon = "Windows";
+                    break;
+
+                case 'l':
+                    //console.log("Linux");
+                    platformIcon = "Linux";
+                    break;
+
+                case 'cp': 
+                  //console.log("Cross-platform");
+                    platformIcon = "Cross-Platform";
+                    break;
+
               }
 
-              // What is happening: this is (note "blink" is first) doing a "as long as there are characters in the word 'blink', loop through ... that is not the loop i want 
 
-              softwareOutput.insertAdjacentHTML("beforeend", "<div class=\"text-primary\">" + eachSubCat + "</div>");
+              softwareOutput.insertAdjacentHTML (
+                'beforeend',
+                '<div class="card border border-dark mb-3">' + titleDisplay + '<div class="card-text">' + description + '<br />Platform:' + platformIcon +  '<br />' + wineDisplay +
+                '</div></div>');
 
             }
           }
-          break;
-
         }
-
-
+        break;
+      }
     }); // ends getJSON loop
   });
 }
