59c4f8bf46bb5a9f970255d869a94c07df7e9b24
Xmetalfanx <Xmetalfanx@yahoo.com>
2019-09-01 17:18:07 -0400

    Attempted (come back to) Browser Section work

---
+++ techHQ/software/internet/browsers.php
@@ -0,0 +1,14 @@
+<?php
+
+  $pageTitle = 'Internet Related Programs - Web Browsers';
+
+  $sitePath = "/home/xmetalx1/public_html";
+  include $sitePath . "/techHQ/modules/programs.php" ;
+  include $sitePath . "/templates/tech/baseTechSoftwareTemp.tpl";
+
+?>
+
+<script type="text/javascript">
+  var localMainCat = "internet";
+  var localSubCat = 'browsers';
+</script>
