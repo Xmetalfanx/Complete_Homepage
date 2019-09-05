8e0cbe56d719757b62395d61d3b5b554e1d5d215
Xmetalfanx <Xmetalfanx@yahoo.com>
2019-09-05 14:57:42 -0400

    JSON updating

--- techHQ/software/security/firewalls.php
+++ techHQ/software/security/firewalls.php
@@ -1,12 +1,11 @@
 <?php
 
-		$the_title= 'Security Section - Software Firewall';
-		$sitePath = "/home/xmetalx1/public_html";
-	  include $sitePath . "/templates/tech/baseTechSoftwareTemp.tpl";
+	$the_title= 'Security Section - Software Firewall';
+	$sitePath = "/home/xmetalx1/public_html";
+	include $sitePath . "/templates/tech/baseTechSoftwareTemp.tpl";
 ?>
 
-
-      <p>
-      <a href="https://www.techsupportalert.com/best-free-firewall.htm" target="_blank">*** PLEASE CHECK OUT GIZMO&#39;S SITE AND COMMENTS ON FIREWALLS, for more information and reviews on firewalls</a> ***</p>
-			</div>
-
+<script type="text/javascript">
+  var localMainCat = 'security';
+  var localSubCat = 'firewalls';
+</script>
---
+++ techHQ/software/security/threatDetection.php
@@ -0,0 +1,11 @@
+<?php
+
+      $the_title= 'Xmetalfanx Security Section - Backing up updates from Avast, Avira, and Malwarebytes via Bat File and 7-zip';
+      $sitePath = "/home/xmetalx1/public_html";
+      include $sitePath . "/templates/tech/baseTechSoftwareTemp.tpl";
+?>
+
+<script type="text/javascript">
+  var localMainCat = 'security';
+  var localSubCat = 'antithreats';
+</script>
