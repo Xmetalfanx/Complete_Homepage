76a6c72201de12b6b5d0f91c0b72222c63787492
Xmetalfanx <Xmetalfanx@yahoo.com>
2019-09-26 22:48:04 -0400

    Bugfix

--- techHQ/index.php
+++ techHQ/index.php
@@ -1,8 +1,9 @@
 <?php
 
-  $pageTitle = "TechHQ Frontpage"
+  $pageTitle = "TechHQ Frontpage";
 
   $path = getenv('DOCUMENT_ROOT');
+
   include $path . '/templates/tech/open/baseTechMAINTemp.tpl';
 
 ?>
@@ -12,25 +13,25 @@
   <div class="card-text px-3">
     <div class="row text-center">
       <div class="col-lg-3 col-md">
-        <a href="https://www.khanacademy.org/" target="_blank">
+        <a href="https://www.khanacademy.org/" target="_blank" rel="noreferrer">
           <img class="mx-auto d-none d-md-block" src="/techHQ/graphics/khan-logo.png" alt="Khan Academy Graphic">Khan Academy</a>
       </div>
       <div class="col-lg-3 col-md">
-        <a href="https://linuxacademy.com/" target="_blank">
+        <a href="https://linuxacademy.com/" target="_blank" rel="noreferrer">
         <img class="mx-auto d-none d-md-block" src="/graphics/linuxAcademy.png" alt="Linux Academy Graphic">Linux Academy</a>
       </div>
       <div class="col-lg-3 col-md">
-        <a href="https://www.digitalocean.com/community/" target="_blank">
+        <a href="https://www.digitalocean.com/community/" target="_blank" rel="noreferrer">
           <img class="mx-auto d-none d-md-block" src="/techHQ/graphics/digital-ocean.png" alt="Digital Ocean Graphic">Digital Ocean</a>
       </div>
       <div class="col-lg-3 col-md">
-        <a href="http://www.kristakingmath.com/" target="_blank">
+        <a href="http://www.kristakingmath.com/" target="_blank" rel="noreferrer">
           <img class="mx-auto d-none d-md-block" src="/graphics/kking_logo.png" alt="Krista King Math Site graphics">Krista King Math Tutor</a>
-        <a href="https://www.youtube.com/channel/UCUDlvPp1MlnegYXOXzj7DEQ">(or Her Channel on Youtube)</a>
+        <a href="https://www.youtube.com/channel/UCUDlvPp1MlnegYXOXzj7DEQ" rel="noreferrer">(or Her Channel on Youtube)</a>
       </div>
     </div>
     <div class="row col-12 px-3 mt-4">
-      <a href="http://www.mathtutordvd.com/" target="_blank">Science and Math.com
+      <a href="http://www.mathtutordvd.com/" target="_blank" rel="noreferrer">Science and Math.com
         <img class="d-none d-1g-block" src="/graphics/mathandscience.jpg" alt="Science and Math.com graphics"></a>
     </div>
   </div>
