85709877cf66bae46eba67c9f739877c84889727
Xmetalfanx <Xmetalfanx@yahoo.com>
2019-09-29 23:07:29 -0400

    Accessibility improvements

--- modules/tech/baseTechFooter.tpl
+++ modules/tech/baseTechFooter.tpl
@@ -1,15 +1,7 @@
 
 <div class="d-none d-md-block ml-4">
   <div class="row p-2">
-    <a class="nav-link" href="/techHQ/index.php">Home</a>
-    
-    <a class="nav-link" href="/techHQ/credit.php">Credits</a>
-    
-    <!-- a.nav-link(href='http://xmetalfanx.x10.mx/phpbb/' target='_blank' rel='noreferrer') Forums-->
-    
-    <!-- a.nav-link(href='#') Site Map commented out due to it being so out off date ... I will update this later  -->
-    
-    <a class="nav-link" href="/techHQ/links.php">Links </a>
+    <a class="nav-link" href="/techHQ/index.php" aria-label="Link to TechHQ Frontpage">Home</a>
     <a class="nav-link" href="#">Contact Me</a>
  </div> <!-- Ends Row -->
 
--- techHQ/index.php
+++ techHQ/index.php
@@ -13,31 +13,31 @@
   <div class="card-text px-3">
     <div class="row text-center">
       <div class="col-lg-3 col-md">
-        <a href="https://www.khanacademy.org/" target="_blank" rel="noreferrer">
+        <a href="https://www.khanacademy.org/" target="_blank" rel="noreferrer" aria-label="Link to Khan Academy">
           <img class="mx-auto d-none d-md-block lazy-load" data-src="/techHQ/graphics/khan-logo.png" alt="Khan Academy Graphic">Khan Academy
         </a>
       </div>
       <div class="col-lg-3 col-md">
-        <a href="https://linuxacademy.com/" target="_blank" rel="noreferrer">
+        <a href="https://linuxacademy.com/" target="_blank" rel="noreferrer" aria-label="Link to Linux Academy">
           <img class="mx-auto d-none d-md-block lazyload" data-src="/graphics/linuxAcademy.png" alt="Linux Academy Graphic">Linux Academy
         </a>
       </div>
       <div class="col-lg-3 col-md">
-        <a href="https://www.digitalocean.com/community/" target="_blank" rel="noreferrer">
+        <a href="https://www.digitalocean.com/community/" target="_blank" rel="noreferrer" aria-label="Link to Digital Ocean">
           <img class="mx-auto d-none d-md-block lazyload"  data-src="/techHQ/graphics/digital-ocean.png" alt="Digital Ocean Graphic">Digital Ocean
         </a>
       </div>
       <div class="col-lg-3 col-md">
-        <a href="http://www.kristakingmath.com/" target="_blank" rel="noreferrer">
+        <a href="http://www.kristakingmath.com/" target="_blank" rel="noreferrer" aria-label="Link to Krista King's Math related site">
           <img class="mx-auto d-none d-md-block lazyload" data-src="/graphics/kking_logo.png" alt="Krista King Math Site graphics">Krista King Math Tutor
         </a>
 
-        <a href="https://www.youtube.com/channel/UCUDlvPp1MlnegYXOXzj7DEQ" rel="noreferrer">(or Her Channel on Youtube)</a>
+        <a href="https://www.youtube.com/channel/UCUDlvPp1MlnegYXOXzj7DEQ" rel="noreferrer" aria-label="Link to Krista King's Youtube page">(or Her Channel on Youtube)</a>
       </div>
     </div>
 
     <div class="row col-12 px-3 mt-4">
-      <a href="http://www.mathtutordvd.com/" target="_blank" rel="noreferrer">Science and Math.com
+      <a href="http://www.scienceandmath.com/" target="_blank" rel="noreferrer" aria-label="Link to ScienceandMath.com">Science and Math.com
         <img class="d-none d-1g-block lazyload" data-src="/graphics/mathandscience.jpg" alt="Science and Math.com graphics">
       </a>
     </div>
