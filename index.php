34034086edd14bc622ccc30a85550c2b77ba7af8
Xmetalfanx <Xmetalfanx@yahoo.com>
2020-01-16 19:04:29 -0500

    tweaks and bugfixes

--- index.php
+++ index.php
@@ -1,47 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title> Xmetal&apos;s Homepage</title><?php include 'modules/headerInclude.tpl'; ?>
-    <!-- Universal CSS file-->
-    <link href="/universalCSS/styling.css" rel="stylesheet" type="text/css" media="all">
+    <title>Xmetal&apos;s Homepage</title><?php include 'modules/headerInclude.tpl'; ?>
   </head>
   <body> 
-    <div class="mx-auto">
-      <!-- Header "navigation"--><?php include 'modules/frontnav.tpl'; ?>
-      <div class="d-none d-lg-block coverImage"></div>
-      <div class="font-weight-bold">October 28th 2018 Update</div>
-      <div>Removed Icon/reference to my old Google+ account which I removed for good earlier tonight ... most people I know on Google+ were going to other platforms anyway, and Google+ is closing for good, soon, so I closed my account down ahead of time </div>
-      <div class="row p-3">
-        
-        <div class="col-md-6 py-2">
-          <div class="card w-75 mx-auto">
-            <div class="card-header">Linux Setup Script</div>
-            <div class="card-body">
-            <a href="https://github.com/Xmetalfanx/linuxSetup" target="_blank" rel="noopener"> 
-              <i class="fa fa-gitlab fa-2x text-dark"></i></a>
-            <a href="https://gitlab.com/Xmetal/linuxSetup" target="_blank" rel="noopener"> 
-              <i class="fa fa-github fa-2x text-dark"></i></a>
-              <br>
-              Helps a user setup some things with a menu system on Ubuntu, Fedora, OpenSuse, and (minor things for this distro ... so far) Solus.   This includes adding well known third party repos, enabling coming setings not set by default, and adding common software in one shot</div>
-          </div>
+    <!-- Header "navigation"-->
+    <?php include 'modules/frontnav.tpl'; ?>
+    <div class="flexContainer">
+      <div class="d-none d-lg-block coverImage flexItem"></div>
+      <div class="row flexItem">
+        <div class="col-6">
+          <div class="font-weight-bold">October 28th 2018 Update</div>
+          <div>Removed Icon/reference to my old Google+ account which I removed for good earlier tonight ... most people I know on Google+ were going to other platforms anyway, and Google+ is closing for good, soon, so I closed my account down ahead of time</div>
         </div>
-        
-        <div class="col-md-6 py-2">
-          <div class="card w-75 mx-auto">
-            <div class="card-header">Linux Themeing Scripts</div>
-            <div class="card-body">
-            <a href="https://github.com/Xmetalfanx/linuxThemes" target="_blank" rel="noopener"> 
-            <i class="fa fa-gitlab fa-2x text-dark"></i></a>
-            
-            <a href="https://gitlab.com/Xmetal/linuxThemes" target="_blank" rel="noopener"> 
-            <i class="fa fa-github fa-2x text-dark"></i></a>
-            
-            <br>
-
-              This originally was going to be a part of the LinuxSetup scripts, though it grew too much and I decided to branch it out.
-              This installs themes and icon packs of well liked Linux Themes into your system.  I have alot of plans for this for version and distro detection.
-            </div>
-          </div>
+        <div class="col-6"><a href="https://github.com/Xmetalfanx" target="_blank" rel="noopener"><i class="fab fa-github fa-2x fa-inverse"></i></a>
+          <div class="font-weight-bold">My Github Other Github project</div><a href="https://github.com/Xmetalfanx/linuxSetup" target="_blank" rel="noopener">LinuxSetup</a>- Helps a user setup some things with a menu system on Ubuntu, Fedora, OpenSuse, and (minor things for this distro ... so far) Solus.   This includes adding well known third party repos, enabling coming setings not set by default, and adding common software in one shot<br><br><a href="https://github.com/Xmetalfanx/linuxThemes" target="_blank" rel="noopener">LinuxThemeing</a>
+           This originally was going to be a part of the LinuxSetup scripts, though it grew too much and I decided to branch it out.
+          This installs themes and icon packs of well liked Linux Themes into your system.  I have alot of plans for this for version and distro detection.
         </div>
       </div>
     </div><?php include 'modules/footer.tpl'; ?>
