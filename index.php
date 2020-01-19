42fec35014fc476919669671463454941477c84b
Xmetalfanx <Xmetalfanx@yahoo.com>
2020-01-19 01:03:25 -0500

    Redid the Frontpage content

--- index.php
+++ index.php
@@ -1,23 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Xmetal&apos;s Homepage</title><?php include 'modules/headerInclude.tpl'; ?>
+    <title>Xmetal&apos;s Homepage</title>
+    <?php include 'modules/headerInclude.tpl'; ?>
   </head>
   <body> 
     <div class="flexContainer">
       <!-- Header "navigation"--><?php include 'modules/frontnav.tpl'; ?>
       <div class="mainSection">
         <div class="d-none d-lg-block coverImage"></div>
-        <div class="row">
-          <div class="col-6">
-            <div class="font-weight-bold">October 28th 2018 Update</div>
-            <div>Removed Icon/reference to my old Google+ account which I removed for good earlier tonight ... most people I know on Google+ were going to other platforms anyway, and Google+ is closing for good, soon, so I closed my account down ahead of time</div>
+        <div class="font-weight-bold">October 28th 2018 Update</div>
+        <div>Removed Icon/reference to my old Google+ account which I removed for good earlier tonight ... most people I know on Google+ were going to other platforms anyway, and Google+ is closing for good, soon, so I closed my account down ahead of time</div>
+        
+        <div class="row py-5">
+          <div class="col-md-6">
+            <div class="card shadow mx-auto w-75">
+              <div class="card-header">Linux Setup </div>
+              <div class="card-text text-justify p-3">
+                <div class="row px-2">
+                  <a href="https://github.com/Xmetalfanx/linuxSetup" target="_blank" rel="noopener"><i class="fa fa-github fa-2x text-dark" aria-hidden="true"></i></a><a href="https://gitlab.com/Xmetal/linuxSetup" target="_blank" rel="noopener"><i class="fa fa-gitlab fa-2x text-dark" aria-hidden="true"></i></a></div>Helps a user setup some things with a menu system on Ubuntu, Fedora, OpenSuse, and (minor things for this distro ... so far) Solus. This includes adding well known third party repos, enabling coming settings not set by default, and adding common software in one shot
+              </div>
+            </div>
           </div>
-          <div class="col-6">
-            <a href="https://github.com/Xmetalfanx" target="_blank" rel="noopener"><i class="fab fa-github fa-2x fa-inverse"></i></a>
-            <div class="font-weight-bold">My Github Other Github project</div><a href="https://github.com/Xmetalfanx/linuxSetup" target="_blank" rel="noopener">LinuxSetup</a>- Helps a user setup some things with a menu system on Ubuntu, Fedora, OpenSuse, and (minor things for this distro ... so far) Solus.   This includes adding well known third party repos, enabling coming setings not set by default, and adding common software in one shot<br><br><a href="https://github.com/Xmetalfanx/linuxThemes" target="_blank" rel="noopener">LinuxThemeing</a>
-             This originally was going to be a part of the LinuxSetup scripts, though it grew too much and I decided to branch it out.
-            This installs themes and icon packs of well liked Linux Themes into your system.  I have alot of plans for this for version and distro detection.
+          <div class="col-md-6"> 
+            <div class="card shadow mx-auto w-75">
+              <div class="card-header">Linux Theming </div>
+              <div class="card-text text-justify p-3">
+                <div class="row px-2">
+                  <a href="https://github.com/Xmetalfanx/linuxThemes" target="_blank" rel="noopener"><i class="fa fa-github fa-2x text-dark" aria-hidden="true"></i></a><a href="https://gitlab.com/Xmetal/linuxThemes" target="_blank" rel="noopener"><i class="fa fa-gitlab fa-2x text-dark" aria-hidden="true"></i></a></div>
+                This originally was going to be a part of the LinuxSetup scripts, though it grew too much and I decided to branch it out.
+                This installs themes and icon packs of well liked Linux Themes into your system.  I have alot of plans for this for version and distro detection.
+              </div>
+            </div>
           </div>
         </div>
       </div>
