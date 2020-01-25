92527531d3983033875f0f7894b4e9f3d707df93
Xmetalfanx <Xmetalfanx@yahoo.com>
2020-01-25 15:37:20 -0500

    Work on main and TechHQ Update (code/display) section

---
+++ modules/siteUpdate.php
@@ -0,0 +1,8 @@
+
+<div class="card">
+  <div class="card-header">Site Update</div>
+  <div class="card-text text-justify px-2">
+    <div class="font-weight-bold">Jan 19, 2020 Update</div>
+    <div class="">After focusing a bit on my Linux Setup Scripts, I am now fixing UI issues, and continuing to complete the site and optimize things where needed.</div>
+  </div>
+</div>
