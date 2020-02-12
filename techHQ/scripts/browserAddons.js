d1ef0f0b2c8c54b028d4b14d45ddbbe83a97f998
Xmetalfanx <Xmetalfanx@yahoo.com>
2020-02-11 21:06:13 -0500

    Browser Addon work

--- techHQ/scripts/browserAddons.js
+++ techHQ/scripts/browserAddons.js
@@ -1,108 +1,123 @@
-let browserResults = document.getElementById("browserSection");
-let browserIconDir = "/techHQ/progIcons/internet/browsers/";
-let addonIconDir = "/techHQ/browsers/addons/graphics/";
+let browserResults = document.getElementById('browserSection');
+let browserIconDir = '/techHQ/progIcons/internet/browsers/';
+let addonIconDir = '/techHQ/browsers/addons/graphics/';
 
 // for Icons
-let firefoxIcon = "${$browserIconDir}/icon8-firefox.svg";
-let chromeIcon =  "${$browserIconDir}/icon8-chrome.svg";
+let firefoxIcon = 'firefox.svg';
+let chromeIcon = 'chrome.svg';
 
 ////////////////////////////////////////////////////////////////////
 // "main" function
 
 function browserAddonData(data) {
+	$(document).ready(function() {
+		// "Things DO GET HERE, successfully"
+		// console.log("Inside softwareData function in JS");
 
-    $(document).ready(function() {
+		browserAddonsJSONUrl = '/techHQ/json/browserAddons.json';
 
-        // "Things DO GET HERE, successfully"
-        // console.log("Inside softwareData function in JS");
+		// Get JSON Data
+		$.getJSON(browserAddonsJSONUrl, function(data) {
 
-        browserAddonsJSONUrl = "/techHQ/json/browserAddons.json";
-
-        // Get JSON Data
-        $.getJSON(browserAddonsJSONUrl, function(data) {
-
-            // For each key value - in this case mainCatagory
             $.each(data, function(key, value) {
-
-                for (var platform in data)
-                {
-                    // Chrome? Firefox? Both? 
-                    let currentAddonPlatform = Object.keys(data[platform]);
-
-                    let currentAddonCat = Object.values(data[platform]);
-
-                    // console.log("currentAddonPlatform:" + currentAddonPlatform);
-
-                    for (var addons in currentAddonCat)
-                    {
-                        let currentPlatformAddon = currentAddonCat[addons];
-
-                        for (var i in currentPlatformAddon)
-                        {
-                            let currentAddon = currentPlatformAddon[i];
-                            let currentAddonString = JSON.stringify(currentAddon);
                 
-                            console.log('currentAddon: ' + currentAddon);
-                            console.log('currentAddonString: ' + currentAddonString);
+                // Chrome Firefox Both
+				let allPlatforms = Object.keys(data);
+				//console.log("allPlatforms: " + allPlatforms);
 
-                            let addonName = currentAddon.addonName;
-                            let addonTitle = currentAddon.addonTitle;
+                // Everything in a platform, including catagories, in (I think) JSON format
 
-                            let browserIcon = currentAddon.iconURL;
+                let AllCats = Object.values(data);
+                //console.log("AllCats: " + AllCats);
 
-                            // let chromeURL = currentAddon.addonURL.chromeURL;
-                            // let firefoxURL = currentAddon.addonURL.firefoxURL;
+				AllCats.forEach(catagories);
 
-                            // Boolean? ... seems logical 
-                            // let ffQuantumSupport = currentAddon.moreInfo.forFFQuantum;
+				function catagories(item, index) {
+                    
+                    // everything including the  catagory 
+                    //let itemString = JSON.stringify(item);
+                    //console.log("itemString: " + itemString);
 
-                            // let developer = currentAddon.moreInfo.developer;
 
-                            const browserAddonOutput = `
+                    let eachCatagory = Object.keys(item);
 
-                            <div class="card">
-                                <div class="card-header">
-                                    <div class="row text-white">
+                    // shows all the addons
+                    let allPlatAddons = Object.values(item);
+                    // console.log(allPlatAddons);
+
+                    allPlatAddons.forEach(cyclePlatformAddons);
+
+                    function cyclePlatformAddons(eachCatsAddons, index)
+                    {
+                        
+                        console.log(eachCatsAddons);
+
+                        eachCatsAddons.forEach(cycleCatagoryAddons);
+
+
+                        function cycleCatagoryAddons(item, index){
+
+                        let eachAddon = item; 
+
+                        let addonTitle = eachAddon.addonTitle;
+                        let addonName = eachAddon.addonName;
+
+                        let addonIcon = eachAddon.iconURL;
+
+                        //
+                        // Meta vars
+                        let moreInfo = eachAddon.moreInfo;
+                        // AddonURLs
+                        // note to self: maybe make the URL, URLs, later ... whatever
+                        let addonURLs = eachAddon.addonURL;
+                        // End of Meta Vars
+
+                        // More Info
+                        let addonDev = moreInfo.developer;
+                        // for Firefox 57+
+                        let addonFFQuat = moreInfo.forFFQuantum;
+
+                        let chromeURL = addonURLs.chromeURL;
+                        let firefoxURL = addonURLs.firefoxURL;
+
+                        const browserOutput = `
+                        
+                            <div class="card shadow my-3">
+                                <div class="card-header"> 
+                                    <div class="row"> 
                                         <div class="col-auto">
-                                            <img src=\"${addonIconDir}${browserIcon}\" alt=\"foobar alt tag\" class=\"iconLG\" />
+                                            <img src=\"${addonIconDir}${addonIcon}\" alt=\"${addonTitle} icon \"  class="iconSM"/>
                                         </div>
-                                        <div class="col-auto">
-                                            <a href="" target="_blank">
-                                                ${addonTitle}
-                                            </a>
-                                        </div>
-                                        <div class="col-auto float-right">
-                                            <a href="#" target="_blank">
-                                                <i class="fab fa-chrome fa-3x"></i>
-                                            </a>
-
-                                            <a href="#" target="_blank">
-                                            <i class="fab fa-firefox fa-3x"></i>
-                                            </a>
-                                        </div>
+                                        <div class="col-auto">${addonTitle}</div>
                                     </div>
                                 </div>
                                 <div class="card-body">
                                     <div>description here, perhaps</div>
+                                    <div class="row py-3">
+                                        <div class="col">
+                                            <a href="${chromeURL}" target="_blank">
+                                                <img src=\"${browserIconDir}${chromeIcon}\" alt=\"${addonTitle} Chrome icon\" class=\" iconLG\" />
+                                            
+                                            </a>
+                                        </div>
+                                        <div class="col">
+                                            <a href="${firefoxURL}" target="_blank">
+                                            <img src=\"${browserIconDir}${firefoxIcon}\" alt=\"${addonTitle} Firefox icon\" class=\" iconLG\" />
+                                            </a>
+                                        </div>
+                                    </div>
 
                                 </div>
                             </div>
-                            `;
-
-                            // Main output
-                            browserResults.insertAdjacentHTML("afterbegin", browserAddonOutput);
+                        `;
+                        
+                        // Main output
+                        browserResults.insertAdjacentHTML('afterbegin', browserOutput);
 
                         }
-
                     }
-
-                }
-
-            }); // Ends .each loop
-
-        }); // ends getJSON loop
-
-
-    });
-
+				}
+			}); // Ends .each loop
+		}); // ends getJSON loop
+	});
 }
