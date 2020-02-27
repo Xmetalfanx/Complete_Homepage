99bb3bef729246fdeaad266ed50f994126de1764
Xmetalfanx <Xmetalfanx@yahoo.com>
2020-02-26 19:56:13 -0500

    How to Install DE code tweaks

--- linuxHQ/scripts/desktops.js
+++ linuxHQ/scripts/desktops.js
@@ -1,12 +1,45 @@
-var desktopResults = document.getElementById('desktopSection');
+var desktopResults = document.getElementById("desktopSection");
 var screenshotDIR = "/linuxHQ/screenshots/";
 
-function displayDEOutput(currentDEName, currentDETitle, currentDEHomepage, currentDEGithub, currentDEScreenshot, currentDELatestVersion, currentDEVerUpdated, currentDECategory, currentDEDistroFeature, currentDEReqMem, currentDEReqHDD, currentDEReqProc, currentDEArchVersion, currentDEF28Version, currentDEF29Version, currentDEFRWVersion, currentDESuseL423Version, currentDESuseL15Version, currentDESuseTWVersion, currentDE1604LTSVer, currentDE1804LTSVer, currentDE1810, currentDEMint173Version, currentDEMint18Version, currentDEMint19Version, currentDEarchInstall, currentDEfedoraInstall, currentDEopensuseInstall, currentDEopensuseLeap423Install, currentDEopensuseLeap15Install, currentDEopensuseTWInstall, currentDEubuntuInstall, currentDEmintInstall) {
+function displayDEOutput(
+  currentDEName,
+  currentDETitle,
+  currentDEHomepage,
+  currentDEGithub,
+  currentDEScreenshot,
+  currentDELatestVersion,
+  currentDEVerUpdated,
+  currentDECategory,
+  currentDEDistroFeature,
+  currentDEReqMem,
+  currentDEReqHDD,
+  currentDEReqProc,
+  currentDEArchVersion,
+  currentDEF28Version,
+  currentDEF29Version,
+  currentDEFRWVersion,
+  currentDESuseL423Version,
+  currentDESuseL15Version,
+  currentDESuseTWVersion,
+  currentDE1604LTSVer,
+  currentDE1804LTSVer,
+  currentDE1810,
+  currentDEMint173Version,
+  currentDEMint18Version,
+  currentDEMint19Version,
+  currentDEarchInstall,
+  currentDEfedoraInstall,
+  currentDEopensuseInstall,
+  currentDEopensuseLeap423Install,
+  currentDEopensuseLeap15Install,
+  currentDEopensuseTWInstall,
+  currentDEubuntuInstall,
+  currentDEmintInstall
+) {
+  // Template Literal for output
 
-    // Template Literal for output
-
-    // used INSIDE the tabs below and not seperately
-    const deVersionsOutput = `
+  // used INSIDE the tabs below and not seperately
+  const deVersionsOutput = `
 
 
             <h3 class="font-weight-bold">Versions</h3>
@@ -84,65 +117,110 @@ function displayDEOutput(currentDEName, currentDETitle, currentDEHomepage, curre
                 </div>
             </div>
 
-
-            <div>
-                <small>Disclaimer: I decided to list the versions currently in different distro/versions of that distro. This means the version of different Desktops that may have SHIPPED WITH the version of the distro listed,may not be the same as what I list. </small>
-            </div>
-
         `;
 
+  const deInstallOutput = `
+    <h3 class="font-weight-bold">How to Install: </h3>
 
-    const deInstallOutput = `
-
-
-            <h3 class="font-weight-bold">How to Install: </h3>
-
-            <div class="font-weight-bold">Arch Linux: </div>
+    <div class="accordion" id="accordionExample">
+        <div class="card">
+        <div class="card-header" id="headingArch">
+            <h2 class="mb-0">
+            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseArch" aria-expanded="true" aria-controls="collapseArch">
+                Arch Linux 
+            </button>
+            </h2>
+        </div>
+    
+      <div id="collapseArch" class="collapse show" aria-labelledby="headingArch" data-parent="#accordionExample">
+        <div class="card-body">
             <code class="my-3 p-2">${currentDEarchInstall}</code>
 
-            <div class="font-weight-bold">Fedora: </div>
-            <code class="my-3 p-2">${currentDEfedoraInstall}</code>
+        </div>
+      </div>
+    </div>
 
-            <div class="font-weight-bold">OpenSUSE: </div>
-                <div class="font-italic">${currentDEopensuseInstall}</div>
-                <br />
+   
+    <div class="card">
+      <div class="card-header" id="headingFedora">
+        <h2 class="mb-0">
+          <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseFedora" aria-expanded="false" aria-controls="collapseFedora">
+            Fedora
+          </button>
+        </h2>
+      </div>
+      <div id="collapseFedora" class="collapse" aria-labelledby="headingFedora" data-parent="#accordionExample">
+        <div class="card-body">
+            Filler Text 
+        </div>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-header" id="headingOpenSuse">
+        <h2 class="mb-0">
+          <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseOpenSuse" aria-expanded="false" aria-controls="collapseOpenSuse">
+            OpenSuse 
+          </button>
+        </h2>
+      </div>
+      <div id="collapseOpenSuse" class="collapse" aria-labelledby="headingOpenSuse" data-parent="#accordionExample">
+        <div class="card-body">
+      
+        <div class="font-weight-bold">OpenSUSE: </div>
+        <div class="font-italic">${currentDEopensuseInstall}</div>
+        <br />
 
-                <small>POSSIBLE Extra Repos (There may or may not be content below)</small>
-                <br />
+        <small>POSSIBLE Extra Repos (There may or may not be content below)</small>
+        <br />
 
-                <div class="font-italic">
-                    Use with Caution ... some of these may be so new they break parts of your DE when updating, but these can have newer versions of DEs then in default repos
-                </div> <br />
+        <div class="font-italic">
+            Use with Caution ... some of these may be so new they break parts of your DE when updating, but these can have newer versions of DEs then in default repos
+        </div> <br />
 
-                <ul>
-                    <li class="font-weight-bold">Leap 42.3</li>
-                        <code class="my-3 p-2">${currentDEopensuseLeap423Install}</code>
-                    <li class="font-weight-bold">Leap 15.0 </li>
-                        <code class="my-3 p-2">${currentDEopensuseLeap15Install}</code>
-                    <li class="font-weight-bold">Tumbleweed</li>
-                        <code class="my-3 p-2">${currentDEopensuseTWInstall}</code>
-                </ul>
+        <ul>
+            <li class="font-weight-bold">Leap 15.0 </li>
+                <code class="my-3 p-2">${currentDEopensuseLeap15Install}</code>
+            <li class="font-weight-bold">Tumbleweed</li>
+                <code class="my-3 p-2">${currentDEopensuseTWInstall}</code>
+        </ul>
+        </div> 
+      </div>
+      <div class="card">
+      <div class="card-header" id="headingUbuntu">
+          <h2 class="mb-0">
+          <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseUbuntu" aria-expanded="true" aria-controls="collapseUbuntu">
+              Linux Mint and/or Ubuntu
+          </button>
+          </h2>
+      </div>
+  </div>
+
+<div id="collapseUbuntu" class="collapse" aria-labelledby="headingUbuntu" data-parent="#accordionExample">
+  <div class="card-body">
+  <ul>
+  <li>Installing on Ubuntu:
+      <code class="my-3 p-2">${currentDEubuntuInstall}</code>
+  </li>
+  <li>Installing on Linux Mint:
+      <code class="my-3 p-2">${currentDEmintInstall}</code>
+  </li>
+</ul>
 
 
-            <div class="font-weight-bold">Ubuntu</div>
-            <ul>
-                <li>Installing on Ubuntu:
-                    <code class="my-3 p-2">${currentDEubuntuInstall}</code>
-                </li>
-                <li>Installing on Linux Mint:
-                    <code class="my-3 p-2">${currentDEmintInstall}</code>
-                </li>
-            </ul>
+</div>
+</div>
+    </div>
+  </div>
         `;
 
-    // Complete Header
-    const deInfoCard = `
+  // Complete Header
+  const deInfoCard = `
 
-            <div class="card">
+            <div class="card my-3 shadow">
                 <div class="card-header bg-primary text-white">
                     <h3 class="font-weight-bold">${currentDETitle}</h3>
                 </div>
-                <div class="card-text px-3 py-2">
+                <div class="card-body">
                     <div class="row">
                         <div class="col">
                             <span class="font-weight-bold">Homepage:</span>
@@ -190,114 +268,146 @@ function displayDEOutput(currentDEName, currentDETitle, currentDEHomepage, curre
 
             `;
 
-    // Main output
-    console.log("currentDEScreenshot: " + currentDEScreenshot);
-    desktopResults.insertAdjacentHTML("beforeend", deInfoCard);
-
+  // Main output
+  console.log("currentDEScreenshot: " + currentDEScreenshot);
+  desktopResults.insertAdjacentHTML("beforeend", deInfoCard);
 }
-
 
 ////////////////////////////////////////////////////////////////////
 // "main" function
 
 // Where is "data" coming from?
 function getDesktopData(data, $localDEName) {
+  $(document).ready(function() {
+    jsonURL = "/linuxHQ/json/desktops.json";
 
-    $(document).ready(function() {
+    // Get JSON Data
+    $.getJSON(jsonURL, function(data) {
+      // For each key value
+      $.each(data, function(key, value) {
+        // The key is 'gtk2' or 'qt'
 
-        jsonURL = "/linuxHQ/json/desktops.json";
+        var currentFramework = key;
 
-        // Get JSON Data
-        $.getJSON(jsonURL, function(data) {
+        for (
+          var subFrameworks = 0;
+          subFrameworks < value.length;
+          subFrameworks++
+        ) {
+          // ** I need to know value and subFrameworks here
 
-            // For each key value
-            $.each(data, function(key, value) {
+          var currentDEName = value[subFrameworks].desktopName;
+          var currentDETitle = value[subFrameworks].desktopTitle;
 
-                // The key is 'gtk2' or 'qt'
+          // More Info
+          var currentDEHomepage =
+            value[subFrameworks].generalInfo.desktopHomepage;
+          var currentDEGithub =
+            value[subFrameworks].generalInfo.desktopGithubURL;
+          var currentDECategory =
+            value[subFrameworks].generalInfo.desktopCatagory;
+          var currentDEDistroFeature =
+            value[subFrameworks].generalInfo.distrosFeature;
 
-                var currentFramework = key;
+          // Requirnments
+          var currentDEReqMem = value[subFrameworks].requirements.reqMemory;
+          var currentDEReqHDD = value[subFrameworks].requirements.reqHDDSpace;
+          var currentDEReqProc = value[subFrameworks].requirements.reqProcessor;
 
-                for (var subFrameworks = 0; subFrameworks < value.length; subFrameworks++) {
+          // Screenshot - just one for now
+          var currentDEScreenshot = value[subFrameworks].screenshots.src;
 
-                    // ** I need to know value and subFrameworks here
+          // Versions
+          var versionJSON = value[subFrameworks].versions;
 
-                    var currentDEName = value[subFrameworks].desktopName;
-                    var currentDETitle = value[subFrameworks].desktopTitle;
+          var currentDELatestVersion = versionJSON.latestVersion;
+          // The point of this is "the last time I checked/updated" even some of the info
+          var currentDEVerUpdated = versionJSON.updated;
 
-                    // More Info
-                    var currentDEHomepage = value[subFrameworks].generalInfo.desktopHomepage;
-                    var currentDEGithub = value[subFrameworks].generalInfo.desktopGithubURL;
-                    var currentDECategory = value[subFrameworks].generalInfo.desktopCatagory;
-                    var currentDEDistroFeature = value[subFrameworks].generalInfo.distrosFeature;
+          // Arch
+          var currentDEArchVersion = versionJSON.arch;
 
-                    // Requirnments
-                    var currentDEReqMem = value[subFrameworks].requirements.reqMemory;
-                    var currentDEReqHDD = value[subFrameworks].requirements.reqHDDSpace;
-                    var currentDEReqProc = value[subFrameworks].requirements.reqProcessor;
+          // Fedora
+          var currentDEF28Version = versionJSON.fedora.f28;
+          var currentDEF29Version = versionJSON.fedora.f29;
+          var currentDEFRWVersion = versionJSON.fedora.rawhide;
 
-                    // Screenshot - just one for now
-                    var currentDEScreenshot = value[subFrameworks].screenshots.src;
+          // OpenSuse
+          var currentDESuseL423Version = versionJSON.opensuse.leap423;
+          var currentDESuseL15Version = versionJSON.opensuse.leap15;
+          var currentDESuseTWVersion = versionJSON.opensuse.tumbleweed;
 
-                    // Versions
-                    var versionJSON = value[subFrameworks].versions;
+          var ubuntuVerJSON = value[subFrameworks].versions.ubuntu;
+          // Linux Mint
+          var currentDEMint173Version = ubuntuVerJSON.mint.mint173;
+          var currentDEMint18Version = ubuntuVerJSON.mint.mint18;
+          var currentDEMint19Version = ubuntuVerJSON.mint.mint19;
 
-                    var currentDELatestVersion = versionJSON.latestVersion;
-                    // The point of this is "the last time I checked/updated" even some of the info
-                    var currentDEVerUpdated = versionJSON.updated;
+          // Ubuntu LTS
+          var currentDE1604LTSVer = ubuntuVerJSON.lts1604ver;
+          var currentDE1804LTSVer = ubuntuVerJSON.lts1804ver;
 
-                    // Arch
-                    var currentDEArchVersion = versionJSON.arch;
+          var currentDE1810 = ubuntuVerJSON.ubuntu1810;
 
-                    // Fedora
-                    var currentDEF28Version = versionJSON.fedora.f28;
-                    var currentDEF29Version = versionJSON.fedora.f29;
-                    var currentDEFRWVersion = versionJSON.fedora.rawhide;
+          // End Version vars
+          ///////////////////////////////////////////
 
-                    // OpenSuse
-                    var currentDESuseL423Version = versionJSON.opensuse.leap423;
-                    var currentDESuseL15Version = versionJSON.opensuse.leap15;
-                    var currentDESuseTWVersion = versionJSON.opensuse.tumbleweed;
+          // Begin Install vars
+          var currentDEarchInstall = value[subFrameworks].install.arch;
+          var currentDEfedoraInstall = value[subFrameworks].install.fedora;
 
-                    var ubuntuVerJSON = value[subFrameworks].versions.ubuntu;
-                    // Linux Mint
-                    var currentDEMint173Version = ubuntuVerJSON.mint.mint173;
-                    var currentDEMint18Version = ubuntuVerJSON.mint.mint18;
-                    var currentDEMint19Version = ubuntuVerJSON.mint.mint19;
+          var openSuseInstallJSON = value[subFrameworks].install.opensuse;
+          var currentDEopensuseInstall = openSuseInstallJSON.opensuseInstall;
+          var currentDEopensuseLeap423Install = openSuseInstallJSON.repoLeap423;
+          var currentDEopensuseLeap15Install = openSuseInstallJSON.repoLeap15;
+          var currentDEopensuseTWInstall = openSuseInstallJSON.repoTW;
 
-                    // Ubuntu LTS
-                    var currentDE1604LTSVer = ubuntuVerJSON.lts1604ver;
-                    var currentDE1804LTSVer = ubuntuVerJSON.lts1804ver;
+          var currentDEubuntuInstall =
+            value[subFrameworks].install.ubuntu.ubuntuInstall;
+          var currentDEmintInstall =
+            value[subFrameworks].install.ubuntu.mintInstall;
 
-                    var currentDE1810 = ubuntuVerJSON.ubuntu1810;
+          ///////////////////////////////////////////////////////////////////////////////////
 
-                    // End Version vars
-                    ///////////////////////////////////////////
-
-                    // Begin Install vars
-                    var currentDEarchInstall = value[subFrameworks].install.arch;
-                    var currentDEfedoraInstall = value[subFrameworks].install.fedora;
-
-                    var openSuseInstallJSON = value[subFrameworks].install.opensuse;
-                    var currentDEopensuseInstall = openSuseInstallJSON.opensuseInstall;
-                    var currentDEopensuseLeap423Install = openSuseInstallJSON.repoLeap423;
-                    var currentDEopensuseLeap15Install = openSuseInstallJSON.repoLeap15;
-                    var currentDEopensuseTWInstall = openSuseInstallJSON.repoTW;
-
-                    var currentDEubuntuInstall = value[subFrameworks].install.ubuntu.ubuntuInstall;
-                    var currentDEmintInstall = value[subFrameworks].install.ubuntu.mintInstall;
-
-                    ///////////////////////////////////////////////////////////////////////////////////
-
-                    if (localDEName == currentDEName) {
-                        displayDEOutput(currentDEName, currentDETitle, currentDEHomepage, currentDEGithub, currentDEScreenshot, currentDELatestVersion, currentDEVerUpdated, currentDECategory, currentDEDistroFeature, currentDEReqMem, currentDEReqHDD, currentDEReqProc, currentDEArchVersion, currentDEF28Version, currentDEF29Version, currentDEFRWVersion, currentDESuseL423Version, currentDESuseL15Version, currentDESuseTWVersion, currentDE1604LTSVer, currentDE1804LTSVer, currentDE1810, currentDEMint173Version, currentDEMint18Version, currentDEMint19Version, currentDEarchInstall, currentDEfedoraInstall, currentDEopensuseInstall, currentDEopensuseLeap423Install, currentDEopensuseLeap15Install, currentDEopensuseTWInstall, currentDEubuntuInstall, currentDEmintInstall);
-                    }
-                }
-
-            }); // Ends .each loop
-
-        }); // ends getJSON loop
-
-
-    });
-
+          if (localDEName == currentDEName) {
+            displayDEOutput(
+              currentDEName,
+              currentDETitle,
+              currentDEHomepage,
+              currentDEGithub,
+              currentDEScreenshot,
+              currentDELatestVersion,
+              currentDEVerUpdated,
+              currentDECategory,
+              currentDEDistroFeature,
+              currentDEReqMem,
+              currentDEReqHDD,
+              currentDEReqProc,
+              currentDEArchVersion,
+              currentDEF28Version,
+              currentDEF29Version,
+              currentDEFRWVersion,
+              currentDESuseL423Version,
+              currentDESuseL15Version,
+              currentDESuseTWVersion,
+              currentDE1604LTSVer,
+              currentDE1804LTSVer,
+              currentDE1810,
+              currentDEMint173Version,
+              currentDEMint18Version,
+              currentDEMint19Version,
+              currentDEarchInstall,
+              currentDEfedoraInstall,
+              currentDEopensuseInstall,
+              currentDEopensuseLeap423Install,
+              currentDEopensuseLeap15Install,
+              currentDEopensuseTWInstall,
+              currentDEubuntuInstall,
+              currentDEmintInstall
+            );
+          }
+        }
+      }); // Ends .each loop
+    }); // ends getJSON loop
+  });
 }
