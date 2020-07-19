8213cd4ea7f965155fe0ea017461c16692422ad5
Xmetalfanx <Xmetalfanx@yahoo.com>
2020-07-19 13:58:14 -0400

    increased color constrast in main righthand menu

--- devScripts/movetoOpenSuseLamp.sh
+++ devScripts/movetoOpenSuseLamp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo rsync  --exclude "node_modules" --exclude "framework" --exclude "database" -rhv * /srv/www/htdocs
+sudo rsync  --exclude "node_modules" --exclude "framework" --exclude "database" -rhv * /srv/http/
 
 echo -e "Done"
