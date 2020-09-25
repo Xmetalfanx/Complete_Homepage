efc81b9d105ecdeeaf15204a3d3eece540d8d75b
Xmetalfanx <Xmetalfanx@yahoo.com>
2020-09-25 15:15:26 -0400

    updated npm packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,36 @@
+'use strict';
+
 var gulp = require('gulp');
-var autoprefixer = require('autoprefixer');
+var sass = require('gulp-sass');
+const autoprefixer = require('autoprefixer');
+const postcss = require('gulp-postcss');
 const prettier = require('gulp-prettier');
+var sorting = require('postcss-sorting');
+var sourcemaps = require('gulp-sourcemaps');
+const cleanCSS = require('gulp-clean-css');
+
+
+sass.compiler = require('node-sass');
 
 // The sass/scss to css gulp option says it's obselute and to be honest ... the vscode autocompiler addon for scss works fine
 
-// Runs Prettier on the CSS file
-gulp.task('prettier', () => {
-  return gulp.src('/universalSCSS/*.css')
-    .pipe(prettier({ singleQuote: true }))
-    .pipe(gulp.dest('/universalCSS'));
+gulp.task('compile', function(){
+  var plugin = [
+    // PostCSS Plugins
+    autoprefixer,
+    sorting
+  ];
+
+  return gulp.src('./universalSCSS/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass()) // Using gulp-sass
+    .pipe(postcss(plugin))
+    .pipe(prettier({}))
+    .pipe(cleanCSS({colors: 'true', format: 'beautify'}))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./universalCSS'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
 });
