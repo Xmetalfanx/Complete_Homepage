56ec838a9a590d58b1265ce10277d96c64e02b41
xmetalfanx <Xmetalfanx@yahoo.com>
2020-10-08 14:19:12 -0400

    refactoring SCSS files

--- linuxHQ/index.php
+++ linuxHQ/index.php
@@ -10,7 +10,7 @@
   <div class="main-section main-section--left-col">
     <?php include $path . '/linuxHQ/modules/mainCarousel.tpl' ?>
 
-    <h4>Introduction</h4>
+    <h4 class="themeColorAccent">Introduction</h4>
 
     <div class="text-justify">
       <p>Hello, I'd like to give a little background about my  experience with Linux, before you start browsing my site, to get a sense for where I am coming from. For a number of years now , I have been using Linux to some degree, and  have been using it more and more, while using  Windows less and less.</p>
