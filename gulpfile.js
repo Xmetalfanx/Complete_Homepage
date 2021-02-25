e6408b73187ba6a861e33e152af5a0338d047960
Xmetalfanx <Xmetalfanx@yahoo.com>
2021-02-25 16:31:12 -0500

    updates to npm and gulp to use dart-sass/sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 'use strict';
 
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var sass = require('gulp-dart-sass');
+
 const autoprefixer = require('autoprefixer');
 const postcss = require('gulp-postcss');
 const prettier = require('gulp-prettier');
@@ -10,7 +11,7 @@ var sourcemaps = require('gulp-sourcemaps');
 const cleanCSS = require('gulp-clean-css');
 
 
-sass.compiler = require('node-sass');
+//sass.render({file: universalSCSS/styling.scss}, function(err, result) { /* ... */ });
 
 // The sass/scss to css gulp option says it's obselute and to be honest ... the vscode autocompiler addon for scss works fine
 
@@ -23,7 +24,7 @@ gulp.task('compile', function(){
 
   return gulp.src('./universalSCSS/**/*.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass()) // Using gulp-sass
+    .pipe(sass().on('error', sass.logError))
     //.pipe(postcss(plugin))
     .pipe(prettier({}))
     //.pipe(cleanCSS({colors: 'true', format: 'beautify'}))
