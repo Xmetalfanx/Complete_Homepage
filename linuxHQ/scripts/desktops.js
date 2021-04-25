6901f336ad5bf399c751f134fe9e853e4545c45b
Xmetalfanx <Xmetalfanx@yahoo.com>
2021-04-25 17:11:27 -0400

    Bugfix

--- linuxHQ/scripts/desktops.js
+++ linuxHQ/scripts/desktops.js
@@ -216,7 +216,7 @@ function displayDEOutput(
 
                         <div class="col d-none d-md-block">
                           <a href="${screenshotDIR}${currentDEScreenshot}" target="_blank">
-                            <img class="d-md-block d-none mx-auto thumbnail-fluid--lg src="${screenshotDIR}${currentDESShotTN}" alt="${currentDESShotDistro} screenshot for ${currentDETitle}" loading="lazy" />
+                            <img class="d-md-block d-none mx-auto thumbnail-fluid--lg" src="${screenshotDIR}${currentDESShotTN}" alt="${currentDESShotDistro} screenshot for ${currentDETitle}" loading="lazy" />
 
                             ${currentDESShotDistro} screenshot for ${currentDETitle}
                           </a>
