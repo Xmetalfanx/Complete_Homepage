bd323754fced528e6b42fd781ce6c1734756cf4f
Xmetalfanx <Xmetalfanx@yahoo.com>
2021-11-18 19:36:09 -0500

    CSS theme tweaking

--- modules/siteSection.php
+++ modules/siteSection.php
@@ -1,27 +1,26 @@
-
 <section class="site-section">
-  <div>
-  <!-- LinuxHQ related with a random image for UI-->
-    <figure>
-      <img class="img-fluid d-none d-lg-inline" src="/graphics/unsplash/city1.jpg" alt="unsplash city image">
+  <section>
+    <figure class="d-none d-lg-inline">
+      <img class="img-fluid" src="/graphics/unsplash/city1.jpg" alt="unsplash city image">
     </figure>
-    <a class="h3" href="/linuxHQ/index.php">LinuxHQ Section </a>
+    <div class="h3 section-title">
+      <a href="/linuxHQ/index.php">LinuxHQ Section </a>
+    </div>
     <p>Linux Distribution and Desktop information, including: Description, Version info, screenshots, Theme and Icon information for those wanting to customize their system's appearance, and more.</p>
-    
-       
-  </div>
+  </section>
 
-  <div>
-  <!-- TechHQ section related with random image for UI-->
-    <a class="h3" href="/techHQ/index.php">TechHQ Section</a>
-    <p>Software suggestions and Web Browser/Browser Addon recommendations -Including cross-platform options for Windows and Linux , and Security tips to keep your computer secure</p> 
-    
-    <figure>
-      <img class="img-fluid figimg" src="/graphics/unsplash/eugene-chystiakov-v3KR1I_qMe0-unsplash.jpg" alt="unsplash lake image">
+  <section>
+    <div class="h3 section-title">
+      <a href="/techHQ/index.php">TechHQ Section</a>
+    </div>
+    <p>Software suggestions and Web Browser/Browser Addon recommendations -Including cross-platform options for Windows and Linux , and Security tips to keep your computer secure</p>
+
+    <figure class="d-none d-lg-inline">
+      <img class="img-fluid" src="/graphics/unsplash/eugene-chystiakov-v3KR1I_qMe0-unsplash.jpg" alt="unsplash lake image">
       <figcaption>
-        Photo by <a href="https://unsplash.com/@eugenechystiakov?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Eugene Chystiakov</a> on <a href="https://unsplash.com/t/architecture?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> 
+        Photo by <a href="https://unsplash.com/@eugenechystiakov?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Eugene Chystiakov</a> on <a href="https://unsplash.com/t/architecture?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
       </figcaption>
     </figure>
-    
-  </div>
+
+  </section>
 </section>
