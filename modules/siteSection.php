1b5c4835ea6f7c5365ad80ba405f20c77bacc107
Xmetalfanx <Xmetalfanx@yahoo.com>
2021-11-26 23:55:42 -0500

    Changing to Christmas theming

--- modules/siteSection.php
+++ modules/siteSection.php
@@ -1,10 +1,10 @@
 <section class="site-section">
   <section>
     <figure class="d-none d-lg-inline">
-      <img class="img-fluid" src="/graphics/unsplash/jed-owen-NPBnWE1o07I-unsplash.jpg" alt="unsplash city image">
-      <figcaption>
-      Photo by <a href="https://unsplash.com/@jediahowen?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jed Owen</a> on <a href="https://unsplash.com/s/photos/thanksgiving?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-      </figcaption>
+      <img class="img-fluid" src="/graphics/unsplash/christmas/tessa-rampersad-5toRIWF2GUY-unsplash.jpg"  alt="unsplash city image">
+    <figcaption>
+      Photo by <a href="https://unsplash.com/@t_rampersad?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Tessa Rampersad</a> on <a href="https://unsplash.com/s/photos/christmas?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+    </figcaption>
     </figure>
     <div class="h3 section-title">
       <a href="/linuxHQ/index.php">LinuxHQ Section </a>
@@ -19,9 +19,9 @@
     <p>Software suggestions and Web Browser/Browser Addon recommendations -Including cross-platform options for Windows and Linux , and Security tips to keep your computer secure</p>
 
     <figure class="d-none d-lg-inline">
-      <img class="img-fluid" src="/graphics/unsplash/joseph-gonzalez-E-EEQSjDdck-unsplash.jpg" alt="unsplash lake image">
+      <img class="img-fluid" src="/graphics/unsplash/christmas/wonderlane-ADeF0nyAvLo-unsplash.jpg" alt="unsplash Christmas image">
       <figcaption>
-      Photo by <a href="https://unsplash.com/@miracletwentyone?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Joseph Gonzalez</a> on <a href="https://unsplash.com/s/photos/thanksgiving?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
       </figcaption>
     </figure>
 
