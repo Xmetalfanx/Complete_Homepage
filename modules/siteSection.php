e5f55499a2330e22a8dc2e93c01475cec1333cef
Xmetalfanx <Xmetalfanx@yahoo.com>
2022-08-21 17:06:31 -0400

    site section updates

--- modules/siteSection.php
+++ modules/siteSection.php
@@ -1,28 +1,25 @@
+
 <section class="site-section">
   <section>
     <figure class="d-none d-lg-inline">
-      <img class="img-fluid" src="/graphics/unsplash/christmas/tessa-rampersad-5toRIWF2GUY-unsplash.jpg"  alt="unsplash city image">
-    <figcaption>
-      Photo by <a href="https://unsplash.com/@t_rampersad?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Tessa Rampersad</a> on <a href="https://unsplash.com/s/photos/christmas?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-    </figcaption>
+      <img class="img-fluid" src="/graphics/unsplash/city1.jpg" alt="unsplash city image">
+      <figcaption></figcaption>
     </figure>
-    <div class="h3 section-title">
-      <a href="/linuxHQ/index.php">LinuxHQ Section </a>
-    </div>
+    <div class="h3 section-title"><a href="/linuxHQ/index.php">LinuxHQ Section</a></div>
+
     <p>Linux Distribution and Desktop information, including: Description, Version info, screenshots, Theme and Icon information for those wanting to customize their system's appearance, and more.</p>
   </section>
 
   <section>
     <div class="h3 section-title">
-      <a href="/techHQ/index.php">TechHQ Section</a>
+      <a href="/techHQ/index.php">TechHQ Section</a></div>
+    <div class="d-none d-lg-inline">
+      Software suggestions and Web Browser/Browser Addon recommendations -Including cross-platform options for Windows and Linux , and Security tips to keep your computer secure.
     </div>
-    <p>Software suggestions and Web Browser/Browser Addon recommendations -Including cross-platform options for Windows and Linux , and Security tips to keep your computer secure</p>
 
     <figure class="d-none d-lg-inline">
-      <img class="img-fluid" src="/graphics/unsplash/christmas/wonderlane-ADeF0nyAvLo-unsplash.jpg" alt="unsplash Christmas image">
-      <figcaption>
-
-      </figcaption>
+      <img class="img-fluid" src="/graphics/unsplash/lake1.jpg" alt="unsplash lake image">
+      <figcaption></figcaption>
     </figure>
 
   </section>
