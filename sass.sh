1592859cf8d91274cfd27e580d751b3915ae3051
Xmetalfanx <Xmetalfanx@yahoo.com>
2023-05-07 13:53:45 -0400

    New site theme
    - not saying this is it's final form, but its a good change for now

--- sass.sh
+++ sass.sh
@@ -17,26 +17,24 @@ npm run fix:scss
 #userPrompt
 npm run compile:scss
 userPrompt
-npm run fix:rawcss
+#npm run fix:rawcss
 
 clear && echo "Running PostCSS on RAW CSS file"
 npm run postcss:all
-userPrompt
+#userPrompt
 
 clear && echo "Fixing output CSS with Stylelint"
 npm run fix:postcss
-userPrompt
-
+#userPrompt
 
 echo "Minifying CSS"
 npm run minify:css
-userPrompt
 
 echo "COMPLETED"
 userPrompt
 
-
 clear
 echo "Performing Cleanup"
 rm css/styling-raw.css*
+rm css/styling-postcss.css*
 echo "Cleanup complete"
