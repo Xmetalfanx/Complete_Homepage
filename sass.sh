190612552b23b431f1f0adaf91330bfee3055b89
Xmetalfanx <Xmetalfanx@yahoo.com>
2023-05-08 15:39:32 -0400

    Media Query refactor - switching to context notation

--- sass.sh
+++ sass.sh
@@ -23,18 +23,12 @@ clear && echo "Running PostCSS on RAW CSS file"
 npm run postcss:all
 #userPrompt
 
-# clear && echo "Fixing output CSS with Stylelint"
-# npm run fix:postcss
-# #userPrompt
-
-# echo "Minifying CSS"
-# npm run minify:css
 
 echo "COMPLETED"
 userPrompt
 
-# clear
-# echo "Performing Cleanup"
-# rm css/styling-raw.css*
-# rm css/styling-postcss.css*
-# echo "Cleanup complete"
+clear
+echo "Performing Cleanup"
+rm css/styling-raw.css*
+rm css/styling-postcss.css*
+echo "Cleanup complete"
