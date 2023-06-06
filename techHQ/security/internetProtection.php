ff9297c706ba49a7b896515f8081c0e0dc20f10d
Xmetalfanx <Xmetalfanx@yahoo.com>
2023-06-06 17:36:52 -0400

    Updates to TechHQ Security sections

--- techHQ/security/internetProtection.php
+++ techHQ/security/internetProtection.php
@@ -41,28 +41,6 @@
 </div>
 
 
-<div class="card w-90">
-  <div class="card__header">Here is a general guide to protection yourself online.</div>
-  <div class="card__body">
-    <ol>
-      <li>DO NOT ... under-estimate common sense</li>
-      <li><a href="#goodAntivirus">Use a good Antivirus program and have a good second opinion scanner</a></li>
-      <li>Web Browser Addons</li>
-      <li>REMEMBER ... just because you are seeing something that you are not familiar with ... doesn't mean "someone is hacking you".</li>
-      <ul>
-        <li>I have seen users with that mindset and it's hard to say troubleshoot a tech issue when they are CONVINCED THEY are being hacked, when the real issue is say a missing driver or some setting getting misconfigured. HAVING SAID THAT...it is better to be safe than sorry, but ... again we are back to "use common sense" here.</li>
-      </ul>
-    </ol>
-  </div>
-</div>
-<div class="card w-90">
-  <div class="card__header" id="goodAntivirus">Use a good Antivirus and have a good Second Opinion Scanner</div>
-  <div class="card__body">
-    <a href="/techHQ/security/infectionProtection.php">My Infection Protection page covering the subject</a>
-    <br/><br/>
-    Second Opinion Scanners are good lines of defense, sometimes picking up or handling infections that some Anti-viruses may miss
-  </div>
-</div>
 <?php include $siteRoot . "/techHQ/security/modules/securityBrowserAddons.php"; ?>
 
 <!-- needed -->
--- techHQ/security/modules/securityBrowserAddons.php
+++ techHQ/security/modules/securityBrowserAddons.php
@@ -1,59 +1,48 @@
 
 <div class="header">Web Browser Addons</div>
-<div class="d-flex flex-dir-columns">
-  <div class="card w-50 mx-auto border--dark" id="httpsEverywhere">
+<div class="cardContainer">
+  <div class="card w-40 border--dark" id="httpsEverywhere">
     <div class="card__header">
-      <div class="row w-100">
-        <div class="col"><img class="icon--size40" src="/techHQ/browsers/addons/graphics/HTTPSEverywhere.svg" alt="HTTPS Everywhere icon ">
-          <div>HTTPS Everywhere</div>
-        </div>
-        <div class="col"><a href="https://chrome.google.com/webstore/detail/https-everywhere/gcbommkclmclpchllfjekcdonpmejbdp?utm_source=chrome-ntp-icon" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/chrome.svg" alt="HTTPS Everywhere Chrome icon"><a href="https://addons.mozilla.org/en-GB/firefox/addon/https-everywhere/" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/firefox.svg" alt="HTTPS Everywhere Firefox icon"></div>
+      <div><img class="icon--size40" src="/techHQ/browsers/addons/graphics/HTTPSEverywhere.svg" alt="HTTPS Everywhere icon ">
+        <div>HTTPS Everywhere</div>
       </div>
+      <div><a href="https://chrome.google.com/webstore/detail/https-everywhere/gcbommkclmclpchllfjekcdonpmejbdp?utm_source=chrome-ntp-icon" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/chrome.svg" alt="HTTPS Everywhere Chrome icon"><a href="https://addons.mozilla.org/en-GB/firefox/addon/https-everywhere/" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/firefox.svg" alt="HTTPS Everywhere Firefox icon"></div>
     </div>
     <div class="card__body">
       <div>*(maybe depreciated with some https features coming to different browsers )</div>
     </div>
   </div>
-  <div class="card w-50 mx-auto border--dark" id="privacyBadger">
+  <div class="card w-40 border--dark" id="privacyBadger">
     <div class="card__header"> 
-      <div class="row w-100">
-        <div class="col"><img class="icon--size40" src="/techHQ/browsers/addons/graphics/privacyBadger.svg" alt="Privacy Badger icon ">
-          <div>Privacy Badger</div>
-        </div>
-        <div class="col"> <a href="https://chrome.google.com/webstore/detail/privacy-badger/pkehgijcmpdhfbdbbnkijodmdjhbjlgp?hl=en-US" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/chrome.svg" alt="Privacy Badger Chrome icon"><a href="https://addons.mozilla.org/en-US/firefox/addon/privacy-badger17/?src=search" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/firefox.svg" alt="Privacy Badger Firefox icon"></div>
+      <div><img class="icon--size40" src="/techHQ/browsers/addons/graphics/privacyBadger.svg" alt="Privacy Badger icon ">
+        <div>Privacy Badger</div>
       </div>
+      <div><a href="https://chrome.google.com/webstore/detail/privacy-badger/pkehgijcmpdhfbdbbnkijodmdjhbjlgp?hl=en-US" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/chrome.svg" alt="Privacy Badger Chrome icon"><a href="https://addons.mozilla.org/en-US/firefox/addon/privacy-badger17/?src=search" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/firefox.svg" alt="Privacy Badger Firefox icon"></div>
     </div>
     <div class="card__body">     
       <div>description here, perhaps</div>
     </div>
   </div>
-  <div class="card w-50 mx-auto border--dark" id="ublockO">
+  <div class="card w-40 border--dark" id="ublockO">
     <div class="card__header">
-      <div class="row w-100">
-        <div class="col">
-          <img class="icon--size40" src="/techHQ/browsers/addons/graphics/ublockOrigin.svg" alt="Ublock Origin icon ">
-          <div>Ublock Origin</div>
-        </div>
-        <div class="col"><a href="https://chrome.google.com/webstore/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm?utm_source=chrome-ntp-icon" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/chrome.svg" alt="Ublock Origin Chrome icon"><a href="https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/?src=search" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/firefox.svg" alt="Ublock Origin Firefox icon"></div>
+      <div><img class="icon--size40" src="/techHQ/browsers/addons/graphics/ublockOrigin.svg" alt="Ublock Origin icon ">
+        <div>Ublock Origin</div>
       </div>
+      <div> <a href="https://chrome.google.com/webstore/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm?utm_source=chrome-ntp-icon" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/chrome.svg" alt="Ublock Origin Chrome icon"><a href="https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/?src=search" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/firefox.svg" alt="Ublock Origin Firefox icon"></div>
     </div>
     <div class="card__body">
       <div>description here, perhaps</div>
     </div>
   </div>
-  <div class="card w-50 mx-auto border--dark" id="umatrix">
-    <div class="card__header"> 
-      <div class="row w-100">
-        <div class="col">
-          <img class="icon--size40" src="/techHQ/browsers/addons/graphics/umatrix.png" alt="Umatrix icon">
-          <div>Umatrix</div>
-        </div>
-        <div class="col"><a href="https://chrome.google.com/webstore/detail/umatrix/ogfcmafjalglgifnmanfmnieipoejdcf?utm_source=chrome-ntp-icon" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/chrome.svg" alt="Umatrix Chrome icon"><a href="https://addons.mozilla.org/en-US/firefox/addon/umatrix/?src=search" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/firefox.svg" alt="Umatrix Firefox icon"></div>
+  <div class="card w-40 border--dark" id="umatrix">
+    <div class="card__header">
+      <div><img class="icon--size40" src="/techHQ/browsers/addons/graphics/umatrix.png" alt="Umatrix icon">
+        <div>Umatrix</div>
       </div>
+      <div><a href="https://chrome.google.com/webstore/detail/umatrix/ogfcmafjalglgifnmanfmnieipoejdcf?utm_source=chrome-ntp-icon" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/chrome.svg" alt="Umatrix Chrome icon"><a href="https://addons.mozilla.org/en-US/firefox/addon/umatrix/?src=search" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/firefox.svg" alt="Umatrix Firefox icon"></div>
     </div>
     <div class="card__body">
       <div>description here, perhaps</div>
-      <div class="row py-3 w-50 mx-auto"></div>
     </div>
   </div>
 </div>
