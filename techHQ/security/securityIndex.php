50fb1370d0a9f1211357b8c6717e4b1f2a32ba9a
Xmetalfanx <Xmetalfanx@yahoo.com>
2023-06-07 15:17:16 -0400

    updates to some techHQ security sections

--- techHQ/security/securityIndex.php
+++ techHQ/security/securityIndex.php
@@ -17,36 +17,36 @@
 
 ?>
 
+
 <h2>Security Related Items</h2>
 <p>Remember - Just because your anti-virus or malware scanner says you:</p>
 <ul>
   <li>"Are infected" does not make it true, if it is a poor scanner</li>
   <li>"Are not infected" does not mean you are not, again ... if its a poor scanner</li>
 </ul>
-
-
 <h3>Overall Security Tips</h3>
-<div class="my-4 card w-80">
-  <div class="card__header">COMMON SENSE</div>
-  <div class="card__body">
-    While I'd never say "forget security software, just use common sense alone".... do not underestimate this.
-    Sure SOME products are "snake oil", but there are some trusted names in the fields of different sorts of security products
-  </div>
-</div>
+<ol> 
+  <li> 
+    <strong>COMMON SENSE</strong>
+    <div>While I'd never say "forget security software, just use common sense alone".... do not underestimate this.</div>Sure SOME products are "snake oil", but there are some trusted names in the fields of different sorts of security products
+  </li>
+  <li><strong>Use a Good Anti-Virus</strong>
+    <div>Here are two good, free options:</div>
+    <div class="row">
+      <div class="col"> 
+        <ul>
+          <li> <a href="https://support.kaspersky.com/us/kscloud" target="_blank"><img class="icon--size32 mr-2" src="/techHQ/progIcons/security/AVs/kaspersky.png" alt="Kaspersky Security Cloud graphics">Kaspersky Security Cloud</a></li>
+        </ul>
+      </div>
+      <div class="col">
+        <ul>
+          <li> <a href="https://www.bitdefender.com/solutions/free.html" target="_blank"><img class="icon--size32 mr-2" src="/techHQ/progIcons/security/AVs/bitDefender.png" alt="Bit Defender Free graphic">Bit Defender Free</a></li>
+        </ul>
+      </div>
+    </div>
+  </li>
+</ol>
 
-<div class="my-4 card w-80">
-  <div class="card__header">Use a Good Anti-Virus</div>
-  <div class="card__body">Here are two good, free options:
-    <ul class="list-group list-group-flush">
-      <li class="list-group-item">
-        <a href="https://support.kaspersky.com/us/kscloud" target="_blank">
-        <img class="icon icon--size48" src="/techHQ/progIcons/security/AVs/kaspersky.png" alt="Kaspersky Security Cloud graphics">Kaspersky Security Cloud</a></li>
-      <li class="list-group-item">
-        <a href="https://www.bitdefender.com/solutions/free.html" target="_blank">
-        <img class="icon icon--size48" src="/techHQ/progIcons/security/AVs/bitDefender.png" alt="Bit Defender Free graphic">Bit Defender Free</a></li>
-    </ul>
-  </div>
-</div>
 
 <?php
   include $siteRoot . "/techHQ/security/sections/malwareScanners.php";
@@ -55,6 +55,3 @@
 
 </div>
 <!-- </div> -->
-
-
-<?php include($siteRoot . '/modules/baseTechFooter.tpl'); ?>
