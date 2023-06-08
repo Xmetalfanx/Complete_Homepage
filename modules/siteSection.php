63f82327686cb83fe47a1a1432cfe106557bff3d
Xmetalfanx <Xmetalfanx@yahoo.com>
2023-06-08 16:50:31 -0400

    Updates to frontpage

--- modules/siteSection.php
+++ modules/siteSection.php
@@ -1,12 +1,11 @@
 
 <div class="site-section">
-  <section>
-    <figure><img class="img-fluid" src="/graphics/unsplash/city1.jpg" alt="unsplash city image">
-      <figcaption></figcaption>
-    </figure>
+  <section> 
+    <figure><img class="img-fluid" src="/graphics/unsplash/landscape/lake1.jpg" alt="unsplash lake image"></figure>
   </section>
   <section>
-    <div class="section-title"><a href="/techHQ/index.php">TechHQ Section</a></div>
+    <div class="section-title">
+      <a href="/techHQ/index.php">TechHQ Section</a></div>
     <div class="section-body"> 
       <div>Software suggestions and Web Browser/Browser Addon recommendations -Including cross-platform options for Windows and Linux , and Security tips to keep your computer secure.</div>
       <div>June 2023 Update: While my intent is the same for this section, I AM behind on keeping up with Windows, since I "left Windows" right at the tail end of Windows 7 days.  I know my program suggestions say for Antivirus and anti-Malware software are up to date (though many of the same apps are just as good as they were years back).  </div>
@@ -14,15 +13,15 @@
     </div>
   </section>
   <section> 
-    <div class="section-title">
-      <a href="/linuxHQ/index.php">LinuxHQ Section</a>
-    </div>
+    <div class="section-title"><a href="/linuxHQ/index.php">LinuxHQ Section</a></div>
     <div class="section-body"> 
       <div>Linux Distribution and Desktop information, including: Description, Version info, screenshots, Theme and Icon information for those wanting to customize their system's appearance, and more.</div>
     </div>
   </section>
-  <section> 
+  <section>
     <figure>
-      <img class="img-fluid" src="/graphics/unsplash/lake1.jpg" alt="unsplash lake image"></figure>
+      <img class="img-fluid" src="/graphics/unsplash/landscape/city1.jpg" alt="unsplash city image">
+      <figcaption></figcaption>
+    </figure>
   </section>
 </div>
