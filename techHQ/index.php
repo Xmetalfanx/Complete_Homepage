3ade87898a28c38d49452f12b70a713e39dcf4b4
Xmetalfanx <Xmetalfanx@yahoo.com>
2023-06-29 17:23:52 -0400

    TechHQ frontpage update and update to graphics assets

--- techHQ/index.php
+++ techHQ/index.php
@@ -19,68 +19,33 @@
 ?>
 
 
-<div class="card educational-sites">
-  <div class="card__header">Educational Sites</div>
-  <div class="card__body d-flex flex-wrap jc-center">
-    <figure>
-      <a href="https://www.khanacademy.org/" target="_blank" rel="noreferrer" aria-label="Link to Khan Academy">
-        <img class="img-fluid" src="/techHQ/graphics/khan-academy.svg" alt="Khan Academy website Graphic" />
-        <figcaption>Khan Academy</figcaption>
-      </a>
-    </figure>
-
-    <figure>
-      <a href="https://www.digitalocean.com/community/" target="_blank" rel="noreferrer" aria-label="Link to Digital Ocean">
-        <img class="img-fluid" src="/techHQ/graphics/digitalOcean.png" alt="Digital Ocean website Graphic" />
-        <figcaption>Digital Ocean</figcaption>
-      </a>
-    </figure>
-
-    <figure>
-      <a href="http://www.kristakingmath.com/" target="_blank" rel="noreferrer" aria-label="Link to Krista King's Math related site">
-        <img class="d-none d-md-block" src="/graphics/kking_logo.png" alt="Krista King Math Site website graphics">
-      <figcaption>Krista King Math Tutor</figcaption>
-      </a>
-    </figure>
-
-    <figure>
-      <a href="http://www.scienceandmath.com/" target="_blank" rel="noreferrer" aria-label="Link to ScienceandMath.com">
-        <img class="d-none d-md-block img-fluid" src="/graphics/mathandscience.png" alt="Science and Math.com website graphics" loading="lazy">
-        <figcaption>ScienceandMath.com</figcaption>
-      </a>
-  </figure>
-
-
-
-  </div> <!-- closes card__body -->
-</div> <!-- closes Edu link, card -->
-
-
-<div class="card span2 w-50">
-  <div class="card__header">Learning Linux</div>
-  <div class="card__body">
-    <h4 class="mb-3">Learning the Commandline</h4>
-
-    <div class="d-flex jc-space-around flex-wrap">
-      <figure>
-        <a href="https://linuxjourney.com/" target="_blank">
-          <img class="d-none d-md-block img-fluid" src="../graphics/linux-journey.png"
-          alt="Graphic link to Linux Journey website" />
-          <figcaption>Linux Journey</figcaption>
+<div class="d-none d-md-flex flex-dir-columns jc-center">
+  <div class="h3 text-center">Educational Sites</div>
+  <div class="list-container">
+    <ul>
+      <li> 
+        <a href="https://www.khanacademy.org/" target="_blank" rel="noreferrer" aria-label="Link to Khan Academy">
+          <img class="img-fluid" src="/techHQ/graphics/khan-academy.svg" alt="Khan Academy website Graphic"></a></li>
+      <li> 
+        <a href="https://www.digitalocean.com/community/" target="_blank" rel="noreferrer" aria-label="Link to Digital Ocean">
+          <img class="img-fluid" src="/techHQ/graphics/digitalOcean.png" alt="Digital Ocean website Graphic"></a></li>
+      <li> 
+        <a href="http://www.scienceandmath.com/" target="_blank" rel="noreferrer" aria-label="Link to ScienceandMath.com">
+          <img class="img-fluid" src="/graphics/mathandscience.png" alt="Science and Math.com website graphics" loading="lazy">
         </a>
-      </figure>
-
-      <figure>
-        <a href="http://www.explainshell.com" target="_blank">
-          <img src="../graphics/explainshell.png" alt="graphic link to Explain Shell.com website" class="d-none d-md-block img-fluid" />
-          <figcaption>ExplainShell.com</figcaption>
-        </a>
-      </figure>
-    </div>
-
-  </div> <!-- closes card__body -->
-
-</div> <!-- closes card -->
+      </li>
+    </ul>
+  </div>
+  
+  <div class="h3 text-center">Learning Linux</div>
+  <div class="h4 text-center">Learning the Commandline</div>
+  <div class="list-container">
+    <ul>
+      <li> <a href="https://linuxjourney.com/" target="_blank"></a><img class="d-none d-md-block img-fluid" src="../graphics/linux-journey.png" alt="Graphic link to Linux Journey website"></li>
+      <li> <a href="http://www.explainshell.com" target="_blank"><img class="d-none d-md-block img-fluid" src="../graphics/explainshell.png" alt="graphic link to Explain Shell.com website"></a></li>
+    </ul>
+  </div>
+</div>
 
 
 <?php include $siteRoot . '/techHQ/modules/gaotd.tpl'; ?>
