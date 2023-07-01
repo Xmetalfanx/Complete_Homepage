3d672da16a0cb98e79737b87327d1125692a523e
Xmetalfanx <Xmetalfanx@yahoo.com>
2023-07-01 14:33:14 -0400

    Site Landing Page Updating
    Frontpage/Landing Page refactoring

--- modules/git.php
+++ modules/git.php
@@ -1,30 +1,32 @@
-<div class="frontpage-title py-3">My Git Projects </div>
-<section class="git-section">
-  <div class="card">
-      <div class="card__header">Linux Setup</div>
-      <div class="card__body">
-        <div class="git-icons">
-          <a href="https://github.com/Xmetalfanx/linuxSetup" target="_blank" rel="noopener" aria-label="Link to Xmetal's Github page for LinuxSetup scripts">
-            <i class="fa fa-3x fa-github" aria-hidden="true"></i>
-          </a>
 
-          <a href="https://gitlab.com/Xmetal/linuxSetup" target="_blank" rel="noopener" aria-label="Link to Xmetal's Gitlab page for LinuxSetup scripts">
-            <i class="fa fa-3x fa-gitlab" aria-hidden="true"></i>
-          </a>
+<div class="git-section">
+  <div class="frontpage-title">My Git Projects </div>
 
-        </div>
-        <p>
-        Helps a user setup some things with a menu system on Ubuntu, Fedora,
-        OpenSuse, and (minor things for this distro ... so far) Solus. </p>
+  <div class="git-cards">
+    <div class="card">
+          <div class="card__header">Linux Setup</div>
+          <div class="card__body">
+            <div class="git-icons">
+              <a href="https://github.com/Xmetalfanx/linuxSetup" target="_blank" rel="noopener" aria-label="Link to Xmetal's Github page for LinuxSetup scripts">
+                <i class="fa fa-3x fa-github" aria-hidden="true"></i>
+              </a>
 
-        <p>This includes adding well known third party repos, enabling coming settings
-        not set by default, and adding common software in one shot.
-        </p>
+              <a href="https://gitlab.com/Xmetal/linuxSetup" target="_blank" rel="noopener" aria-label="Link to Xmetal's Gitlab page for LinuxSetup scripts">
+                <i class="fa fa-3x fa-gitlab" aria-hidden="true"></i>
+              </a>
+
+            </div>
+            <p>
+            Helps a user setup some things with a menu system on Ubuntu, Fedora,
+            OpenSuse, and (minor things for this distro ... so far) Solus. </p>
+
+            <p>This includes adding well known third party repos, enabling coming settings
+            not set by default, and adding common software in one shot.
+            </p>
 
 
+          </div>
       </div>
-  </div>
-
   <div class="card">
     <div class="card__header">Linux Theming</div>
       <div class="card__body">
@@ -40,7 +42,7 @@
             </a>
           </div>
           <p>
-          This originally was going to be a part of the LinuxSetup scripts, though
+            This originally was going to be a part of the LinuxSetup scripts, though
             it grew too much and I decided to branch it out. This installs themes
             and icon packs of well liked Linux Themes into your system. I have alot
             of plans for this for version and distro detection.
@@ -48,4 +50,8 @@
 
       </div>
     </div>
-</section>
+  </div>
+  
+
+
+</div>
--- modules/siteSection.php
+++ modules/siteSection.php
@@ -1,31 +1,19 @@
-
-<div class="site-section">
- 
-<section>
-  <figure>
-    <img class="img-fluid" src="/graphics/unsplash/landscape/city1.jpg" alt="unsplash city image">
-    <figcaption></figcaption>
-  </figure>
-</section>
-<section>
-  <div class="section-title">
-    <a href="/techHQ/index.php">TechHQ Section</a></div>
-  <div class="section-body"> 
-    <div>Software suggestions and Web Browser/Browser Addon recommendations -Including cross-platform options for Windows and Linux, and Security tips to keep your computer secure.</div>
+ <div class="site-sections">
+    <section>
+      <div class="section-title"><a href="/linuxHQ/index.php">LinuxHQ Section</a></div>
+      <div class="section-body">
+         Linux Distribution and Desktop information, including: 
+        <ul>
+          <li>Description, Version info, screenshots,</li>
+          <li>Theme and Icon information for those wanting to customize their system's appearance, and more.</li>
+        </ul>
+      </div>
+    </section>
+    <section>
+      <div class="section-title"><a href="/techHQ/index.php">TechHQ Section</a></div>
+      <div class="section-body">
+         Software suggestions and Web Browser/Browser Addon  recommendations -Including cross-platform options for Windows and Linux, and Security tips to keep your computer secure.</div>
+    </section>
   </div>
-  </section>
-  <section> 
-    <div class="section-title">
-      <a href="/linuxHQ/index.php">LinuxHQ Section</a>
-    </div>
-    <div class="section-body"> 
-      <div>Linux Distribution and Desktop information, including: Description, Version info, screenshots, Theme and Icon information for those wanting to customize their system's appearance, and more.</div>
-    </div>
-  </section>
-   <section> 
-    <figure>
-      <img class="img-fluid" src="/graphics/unsplash/landscape/lake1.jpg" alt="unsplash lake image">
-    </figure>
-  </section> 
-
-</div>
+<div class="frontpage-graphic">
+  <img class="img-fluid" src="/graphics/unsplash/landscape/city1.jpg" alt="unsplash city image"></div>
