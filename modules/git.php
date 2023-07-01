815de53330d941ef1439a997f619c7aa9d6e52dd
Xmetalfanx <Xmetalfanx@yahoo.com>
2023-07-01 17:12:38 -0400

    more frontpage refactoring

--- modules/git.php
+++ modules/git.php
@@ -1,57 +1,29 @@
 
 <div class="git-section">
-  <div class="frontpage-title">My Git Projects </div>
-
-  <div class="git-cards">
-    <div class="card">
-          <div class="card__header">Linux Setup</div>
-          <div class="card__body">
-            <div class="git-icons">
-              <a href="https://github.com/Xmetalfanx/linuxSetup" target="_blank" rel="noopener" aria-label="Link to Xmetal's Github page for LinuxSetup scripts">
-                <i class="fa fa-3x fa-github" aria-hidden="true"></i>
-              </a>
-
-              <a href="https://gitlab.com/Xmetal/linuxSetup" target="_blank" rel="noopener" aria-label="Link to Xmetal's Gitlab page for LinuxSetup scripts">
-                <i class="fa fa-3x fa-gitlab" aria-hidden="true"></i>
-              </a>
-
-            </div>
-            <p>
-            Helps a user setup some things with a menu system on Ubuntu, Fedora,
-            OpenSuse, and (minor things for this distro ... so far) Solus. </p>
-
-            <p>This includes adding well known third party repos, enabling coming settings
-            not set by default, and adding common software in one shot.
-            </p>
-
-
-          </div>
-      </div>
-  <div class="card">
-    <div class="card__header">Linux Theming</div>
-      <div class="card__body">
-        <div class="git-icons">
-          <a href="https://github.com/Xmetalfanx/linuxThemes" target="_blank"
-            rel="noopener" aria-label="Link to Xmetal's Github page for Linux Theming scripts">
-              <i class="fa fa-3x fa-github" aria-hidden="true"></i>
-            </a>
-
-            <a href="https://gitlab.com/Xmetal/linuxThemes"
-              target="_blank" rel="noopener" aria-label="Link to Xmetal's Gitlab page for Linux Theming scripts" >
-                <i class="fa fa-3x fa-gitlab" aria-hidden="true"></i>
-            </a>
-          </div>
-          <p>
-            This originally was going to be a part of the LinuxSetup scripts, though
-            it grew too much and I decided to branch it out. This installs themes
-            and icon packs of well liked Linux Themes into your system. I have alot
-            of plans for this for version and distro detection.
-          </p>
-
-      </div>
+  <div class="frontpage-title">My Git Projects</div>
+  <div class="git-container">
+    <div class="git-group">
+      <div class="git-section-title">Linux Setup  </div>
+      <div class="git-icons"><a href="https://github.com/Xmetalfanx/linuxSetup" target="_blank" rel="noopener" aria-label="Link to Xmetal's Github page for LinuxSetup scripts"><i class="fa fa-3x fa-github" aria-hidden="true"></i></a><a href="https://gitlab.com/Xmetal/linuxSetup" target="_blank" rel="noopener" aria-label="Link to Xmetal's Gitlab page for LinuxSetup scripts"><i class="fa fa-3x fa-gitlab" aria-hidden="true"></i></a></div>
+      <div>Helps a user setup Linux by:</div>
+      <ul>
+        <li>Installing Updates and (when appropriate) Optimizing mirrors</li>
+        <li>Lets the user install various softwares and/or themes</li>
+        <li>Installs/Enables some third party repos that update the software selection they can install</li>
+      </ul>Distro bases supported:
+      <ul class="d-flex flex-wrap">
+        <li>Arch</li>
+        <li>Debian</li>
+        <li>Fedora</li>
+        <li>OpenSUSE</li>
+        <li>Solus</li>
+        <li>Ubuntu</li>
+      </ul>
+    </div>
+    <div class="git-group">
+      <div class="git-section-title">Linux Theming</div>
+      <div class="git-icons"><a href="https://github.com/Xmetalfanx/linuxThemes" target="_blank" rel="noopener" aria-label="Link to Xmetal's Github page for Linux Theming scripts"><i class="fa fa-3x fa-github" aria-hidden="true"></i></a><a href="https://gitlab.com/Xmetal/linuxThemes" target="_blank" rel="noopener" aria-label="Link to Xmetal's Gitlab page for Linux Theming scripts"><i class="fa fa-3x fa-gitlab" aria-hidden="true"></i></a></div>
+      <div>This originally was going to be a part of the LinuxSetup scripts, though it grew too much and I decided to branch it out. This installs themes and icon packs of well liked Linux Themes into your system. I have alot of plans for this for version and distro detection.</div>
     </div>
   </div>
-  
-
-
 </div>
