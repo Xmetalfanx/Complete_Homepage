cfcd32a4335c3d4fb60c28ed68b9c15b97071fd0
Xmetalfanx <Xmetalfanx@yahoo.com>
2023-10-19 16:45:42 -0400

    fix for moved browser addon graphics

--- techHQ/security/modules/securityBrowserAddons.php
+++ techHQ/security/modules/securityBrowserAddons.php
@@ -3,10 +3,11 @@
 <div class="cardContainer">
   <div class="card w-40" id="httpsEverywhere">
     <div class="card__header">
-      <div><img class="icon--size40" src="/techHQ/browsers/addons/graphics/HTTPSEverywhere.svg" alt="HTTPS Everywhere icon ">
+      <div><img class="icon--size40" src="/techHQ/browsers/graphics/icons/HTTPSEverywhere.svg" alt="HTTPS Everywhere icon ">
         <div>HTTPS Everywhere</div>
       </div>
-      <div><a href="https://chrome.google.com/webstore/detail/https-everywhere/gcbommkclmclpchllfjekcdonpmejbdp?utm_source=chrome-ntp-icon" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/chrome.svg" alt="HTTPS Everywhere Chrome icon"><a href="https://addons.mozilla.org/en-GB/firefox/addon/https-everywhere/" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/firefox.svg" alt="HTTPS Everywhere Firefox icon"></div>
+      <div>
+        <a href="https://chrome.google.com/webstore/detail/https-everywhere/gcbommkclmclpchllfjekcdonpmejbdp?utm_source=chrome-ntp-icon" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/chrome.svg" alt="HTTPS Everywhere Chrome icon"><a href="https://addons.mozilla.org/en-GB/firefox/addon/https-everywhere/" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/firefox.svg" alt="HTTPS Everywhere Firefox icon"></div>
     </div>
     <div class="card__body">
       <div>*(maybe depreciated with some https features coming to different browsers )</div>
@@ -14,7 +15,7 @@
   </div>
   <div class="card w-40" id="privacyBadger">
     <div class="card__header"> 
-      <div><img class="icon--size40" src="/techHQ/browsers/addons/graphics/privacyBadger.svg" alt="Privacy Badger icon ">
+      <div><img class="icon--size40" src="/techHQ/browsers/graphics/icons/privacyBadger.svg" alt="Privacy Badger icon ">
         <div>Privacy Badger</div>
       </div>
       <div><a href="https://chrome.google.com/webstore/detail/privacy-badger/pkehgijcmpdhfbdbbnkijodmdjhbjlgp?hl=en-US" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/chrome.svg" alt="Privacy Badger Chrome icon"><a href="https://addons.mozilla.org/en-US/firefox/addon/privacy-badger17/?src=search" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/firefox.svg" alt="Privacy Badger Firefox icon"></div>
@@ -25,7 +26,7 @@
   </div>
   <div class="card w-40" id="ublockO">
     <div class="card__header">
-      <div><img class="icon--size40" src="/techHQ/browsers/addons/graphics/ublockOrigin.svg" alt="Ublock Origin icon ">
+      <div><img class="icon--size40" src="/techHQ/browsers/graphics/icons/ublockOrigin.svg" alt="Ublock Origin icon ">
         <div>Ublock Origin</div>
       </div>
       <div> <a href="https://chrome.google.com/webstore/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm?utm_source=chrome-ntp-icon" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/chrome.svg" alt="Ublock Origin Chrome icon"><a href="https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/?src=search" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/firefox.svg" alt="Ublock Origin Firefox icon"></div>
@@ -36,7 +37,7 @@
   </div>
   <div class="card w-40" id="umatrix">
     <div class="card__header">
-      <div><img class="icon--size40" src="/techHQ/browsers/addons/graphics/umatrix.png" alt="Umatrix icon">
+      <div><img class="icon--size40" src="/techHQ/browsers/graphics/icons/umatrix.png" alt="Umatrix icon">
         <div>Umatrix</div>
       </div>
       <div><a href="https://chrome.google.com/webstore/detail/umatrix/ogfcmafjalglgifnmanfmnieipoejdcf?utm_source=chrome-ntp-icon" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/chrome.svg" alt="Umatrix Chrome icon"><a href="https://addons.mozilla.org/en-US/firefox/addon/umatrix/?src=search" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/firefox.svg" alt="Umatrix Firefox icon"></div>
