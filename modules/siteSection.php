995cff16c610d9efd3a321d6408f4e2a28177440
Xmetalfanx <Xmetalfanx@yahoo.com>
2023-10-22 14:42:02 -0400

    Frontpage redesign

--- modules/siteSection.php
+++ modules/siteSection.php
@@ -1,19 +1,31 @@
- <div class="site-sections">
-    <section>
-      <div class="section-title"><a href="/linuxHQ/index.php">LinuxHQ Section</a></div>
-      <div class="section-body">
-         Linux Distribution and Desktop information, including: 
+
+<div class="site-sections">
+  <section>
+    <div class="section-title">
+      <a href="/linuxHQ/index.php">LinuxHQ Section</a>
+    </div>
+    <div class="section-details">
+      <div class="section-description">
+        Linux Distribution and Desktop information, including: 
         <ul>
           <li>Description, Version info, screenshots,</li>
           <li>Theme and Icon information for those wanting to customize their system's appearance, and more.</li>
         </ul>
       </div>
-    </section>
-    <section>
-      <div class="section-title"><a href="/techHQ/index.php">TechHQ Section</a></div>
-      <div class="section-body">
-         Software suggestions and Web Browser/Browser Addon  recommendations -Including cross-platform options for Windows and Linux, and Security tips to keep your computer secure.</div>
-    </section>
-  </div>
-<div class="frontpage-graphic">
-  <img class="img-fluid" src="/graphics/unsplash/landscape/city1.jpg" alt="unsplash city image"></div>
+      <div class="section-image">
+        <img src="/graphics/frontpage/pumpkin.jpg" alt="Pumpkin image" class="img-fluid">
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="section-title">
+      <a href="/techHQ/index.php">TechHQ Section</a>
+    </div>
+    <div class="section-details">
+      <div class="section-image">
+        <img src="/graphics/frontpage/fall.jpg" alt="Autumn/Fall image" class="img-fluid">
+      </div>
+      <div class="section-description">Software suggestions and Web Browser/Browser Addon  recommendations -Including cross-platform options for Windows and Linux, and Security tips to keep your computer secure.</div>
+    </div>
+  </section>
+</div>
