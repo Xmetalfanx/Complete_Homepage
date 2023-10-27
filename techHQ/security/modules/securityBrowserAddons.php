4312b1b996585080240a1293c165282db176c4d0
Xmetalfanx <Xmetalfanx@yahoo.com>
2023-10-27 17:27:01 -0400

    alignment change for frontpage images

--- techHQ/security/modules/securityBrowserAddons.php
+++ techHQ/security/modules/securityBrowserAddons.php
@@ -1,24 +1,13 @@
 
 <div class="header">Web Browser Addons</div>
 <div class="cardContainer">
-  <div class="card w-40" id="httpsEverywhere">
-    <div class="card__header">
-      <div><img class="icon--size40" src="/techHQ/browsers/graphics/icons/HTTPSEverywhere.svg" alt="HTTPS Everywhere icon ">
-        <div>HTTPS Everywhere</div>
-      </div>
-      <div>
-        <a href="https://chrome.google.com/webstore/detail/https-everywhere/gcbommkclmclpchllfjekcdonpmejbdp?utm_source=chrome-ntp-icon" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/chrome.svg" alt="HTTPS Everywhere Chrome icon"><a href="https://addons.mozilla.org/en-GB/firefox/addon/https-everywhere/" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/firefox.svg" alt="HTTPS Everywhere Firefox icon"></div>
-    </div>
-    <div class="card__body">
-      <div>*(maybe depreciated with some https features coming to different browsers )</div>
-    </div>
-  </div>
+  
   <div class="card w-40" id="privacyBadger">
     <div class="card__header"> 
       <div><img class="icon--size40" src="/techHQ/browsers/graphics/icons/privacyBadger.svg" alt="Privacy Badger icon ">
         <div>Privacy Badger</div>
       </div>
-      <div><a href="https://chrome.google.com/webstore/detail/privacy-badger/pkehgijcmpdhfbdbbnkijodmdjhbjlgp?hl=en-US" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/chrome.svg" alt="Privacy Badger Chrome icon"><a href="https://addons.mozilla.org/en-US/firefox/addon/privacy-badger17/?src=search" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/firefox.svg" alt="Privacy Badger Firefox icon"></div>
+      <div><a href="https://chrome.google.com/webstore/detail/privacy-badger/pkehgijcmpdhfbdbbnkijodmdjhbjlgp?hl=en-US" target="_blank"><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/chrome.svg" alt="Privacy Badger Chrome icon"></a><a href="https://addons.mozilla.org/en-US/firefox/addon/privacy-badger17/?src=search" target="_blank"><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/firefox.svg" alt="Privacy Badger Firefox icon"></a></div>
     </div>
     <div class="card__body">     
       <div>description here, perhaps</div>
@@ -29,7 +18,7 @@
       <div><img class="icon--size40" src="/techHQ/browsers/graphics/icons/ublockOrigin.svg" alt="Ublock Origin icon ">
         <div>Ublock Origin</div>
       </div>
-      <div> <a href="https://chrome.google.com/webstore/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm?utm_source=chrome-ntp-icon" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/chrome.svg" alt="Ublock Origin Chrome icon"><a href="https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/?src=search" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/firefox.svg" alt="Ublock Origin Firefox icon"></div>
+      <div> <a href="https://chrome.google.com/webstore/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm?utm_source=chrome-ntp-icon" target="_blank"><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/chrome.svg" alt="Ublock Origin Chrome icon"></a><a href="https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/?src=search" target="_blank"><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/firefox.svg" alt="Ublock Origin Firefox icon"></a></div>
     </div>
     <div class="card__body">
       <div>description here, perhaps</div>
@@ -40,7 +29,7 @@
       <div><img class="icon--size40" src="/techHQ/browsers/graphics/icons/umatrix.png" alt="Umatrix icon">
         <div>Umatrix</div>
       </div>
-      <div><a href="https://chrome.google.com/webstore/detail/umatrix/ogfcmafjalglgifnmanfmnieipoejdcf?utm_source=chrome-ntp-icon" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/chrome.svg" alt="Umatrix Chrome icon"><a href="https://addons.mozilla.org/en-US/firefox/addon/umatrix/?src=search" target="_blank"></a><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/firefox.svg" alt="Umatrix Firefox icon"></div>
+      <div><a href="https://chrome.google.com/webstore/detail/umatrix/ogfcmafjalglgifnmanfmnieipoejdcf?utm_source=chrome-ntp-icon" target="_blank"><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/chrome.svg" alt="Umatrix Chrome icon"></a><a href="https://addons.mozilla.org/en-US/firefox/addon/umatrix/?src=search" target="_blank"><img class="icon--size40" src="/techHQ/progIcons/internet/browsers/firefox.svg" alt="Umatrix Firefox icon"></a></div>
     </div>
     <div class="card__body">
       <div>description here, perhaps</div>
