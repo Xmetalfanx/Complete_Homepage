9077752e3a02535f119b8876d440a1340a9f62ca
Xmetalfanx <Xmetalfanx@yahoo.com>
2023-11-29 16:41:24 -0500

    Update to site section code

--- modules/siteSection.php
+++ modules/siteSection.php
@@ -1,29 +1,27 @@
 
 <div class="site-sections">
   <section>
-    <div class="section-title">
-      <a href="/linuxHQ/index.php">LinuxHQ Section</a>
-    </div>
-    <div class="section-details">
-      <div class="section-description">
-        Linux Distribution and Desktop information, including: 
+    <div class="section--title"><a href="/linuxHQ/index.php">LinuxHQ Section</a></div>
+    <div class="section--details">
+      <div class="section-description">Linux Distribution and Desktop information, including: 
         <ul>
           <li>Description, Version info, screenshots,</li>
           <li>Theme and Icon information for those wanting to customize their system's appearance, and more.</li>
         </ul>
       </div>
-      <div class="section-image">
-        <img class="mx-auto" src="/graphics/frontpage/pumpkin.jpg" alt="Pumpkin image">
+      <div class="section--image">
+        <img class="mx-auto" src="/graphics/unsplash/christmas/tessa-rampersad-5toRIWF2GUY-unsplash.jpg" alt="Christmas Tree image">
+        <div class="credits">
+          Photo by <a href="https://unsplash.com/@t_rampersad?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Tessa Rampersad</a> on <a href="https://unsplash.com/photos/green-christmas-tree-5toRIWF2GUY?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+        </div>
       </div>
     </div>
   </section>
   <section>
-    <div class="section-title">
-      <a href="/techHQ/index.php">TechHQ Section</a>
-    </div>
-    <div class="section-details">
-      <div class="section-image">
-        <img class="mx-auto" src="/graphics/frontpage/fall.jpg" alt="Autumn/Fall image">
+    <div class="section--title"><a href="/techHQ/index.php">TechHQ Section</a></div>
+    <div class="section--details">
+      <div class="section--image"><img class="mx-auto" src="/graphics/unsplash/christmas/james-wheeler-faMVPE56PnU-unsplash.jpg" alt="Christmas house decoration image">
+        <div class="credits">Photo by <a href="https://unsplash.com/@souvenirpixels?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">James Wheeler</a> on <a href="https://unsplash.com/photos/snowman-and-santa-claus-near-house-decor-faMVPE56PnU?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a></div>
       </div>
       <div class="section-description">Software suggestions and Web Browser/Browser Addon  recommendations -Including cross-platform options for Windows and Linux, and Security tips to keep your computer secure.</div>
     </div>
